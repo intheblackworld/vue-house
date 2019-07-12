1c30f5a296183bfe14f59cc601054c2f68de9e83
Zuray David <intheblackworld@gmail.com>
2019-07-12 21:04:19 +0800

    update address

--- src/info/index.js
+++ src/info/index.js
@@ -1,5 +1,5 @@
 export default {
-  address: '新北市五股區新五路三段二號旁',
+  address: '244新北市林口區文化三路一段 & 忠孝三路',
   googleSrc:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.9735270623682!2d121.35965001500688!3d25.068886383956066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a72933eb3ca1%3A0xcf425b8e84f454a2!2zMjQ05paw5YyX5biC5p6X5Y-j5Y2A5paH5YyW5LiJ6Lev5LiA5q61ICYg5b-g5a2d5LiJ6Lev!5e0!3m2!1szh-TW!2stw!4v1562936205493!5m2!1szh-TW!2stw',
   phone: '02-26095500',
