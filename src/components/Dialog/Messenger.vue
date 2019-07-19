590c7a83f9500b58e1768dec25755189779c8fb9
Zuray David <intheblackworld@gmail.com>
2019-07-19 18:45:04 +0800

    modify mobileNav

--- src/components/Dialog/Messenger.vue
+++ src/components/Dialog/Messenger.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dialog-content">
     <font-awesome-icon :icon="['fab', 'facebook-messenger']" />
-    <div class="dialog-desc">賞屋專線</div>
+    <div class="dialog-desc">Facebook Messenger</div>
     <div class="info">線上諮詢</div>
     <a class="cta" :href="messenger" target="_blank">立即諮詢</a>
   </div>
