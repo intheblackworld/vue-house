45ef7a0b5f4a3ece72b4240000b8f8eb70ef66d9
Zuray David <intheblackworld@gmail.com>
2019-07-19 18:44:26 +0800

    modify mobileNav

--- src/info/index.js
+++ src/info/index.js
@@ -4,7 +4,7 @@ export default {
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.886960330985!2d121.42774591500692!3d25.07182018395455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a63bb7e6ed95%3A0xdb1573ce2f206c43!2zMjQ45paw5YyX5biC5LqU6IKh5Y2A5rC056KT5LqU6LevODHomZ8!5e0!3m2!1szh-TW!2stw!4v1563020603251!5m2!1szh-TW!2stw',
   phone: '02-22918890',
   fbLink: 'https://m.facebook.com/心天畝-315443315712532/?ref=bookmarks',
-  fbMessage: 'm.me/315443315712532',
+  fbMessage: 'https://m.me/315443315712532',
   googleLink: 'https://goo.gl/maps/d4mpi3WPdJ2kLvsF7',
   caseName: '心天畝',
 
