77180f47e927dec31d390cea0a83fc1c89cfa6bd
Zuray David <intheblackworld@gmail.com>
2019-08-21 16:22:18 +0800

    modify phone

--- src/info/index.js
+++ src/info/index.js
@@ -2,7 +2,7 @@ export default {
   address: '新北市林口區文化三路一段 & 忠孝三路',
   googleSrc:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.468070922495!2d120.18393571546702!3d23.00657998496119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e7641bf4667cd%3A0x40c8a3d577f8a8b5!2zU1RBUkJVQ0tTIOaYn-W3tOWFiyAo5rmW576O6ZaA5biCKQ!5e0!3m2!1szh-TW!2stw!4v1565866927036!5m2!1szh-TW!2stw',
-  phone: '06-350-8787',
+  phone: '02-26095500',
   fbLink: 'https://www.facebook.com/蘭桂坊-102691431093421/',
   fbMessage: 'https://m.me/102691431093421/',
   googleLink: 'https://goo.gl/maps/nSYjM15fL514PSRQ9',
