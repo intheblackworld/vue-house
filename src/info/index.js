5a04e3b5302e93286fd7276aa1624cc0feff889a
Zuray David <intheblackworld@gmail.com>
2019-09-02 11:22:17 +0800

    modify info

--- src/info/index.js
+++ src/info/index.js
@@ -1,23 +1,22 @@
 export default {
-  address: '高雄市楠梓區藍田路大學七街 交叉口',
+  address: '新北市三峽區中山路459巷92號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3680.0152705871374!2d120.28651681496243!3d22.72767378510322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e0f320b5498a3%3A0x2c9eeeaadf780327!2z5aSn5a245aKFIOaOpeW-heS4reW_gw!5e0!3m2!1szh-TW!2stw!4v1567323808325!5m2!1szh-TW!2stw',
-  phone: '07-366-3888',
-  fbLink: 'https://www.facebook.com/sunyoungthebestvilla/',
-  fbMessage: 'https://m.me/sunyoungthebestvilla/',
-  googleLink: 'https://goo.gl/maps/iZMGuC4QLJpcJUSTA',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3617.8875157512216!2d121.35171201500431!3d24.935901584018335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3468195875d82cab%3A0xdce4424bb186ce36!2zMjM35paw5YyX5biC5LiJ5bO95Y2A5Lit5bGx6LevNDU55be3OTLomZ8!5e0!3m2!1szh-TW!2stw!4v1567394439545!5m2!1szh-TW!2stw',
+  phone: '02-26737555',
+  fbLink: 'https://www.facebook.com/MoreThanTheMountain/',
+  fbMessage: 'https://m.me/MoreThanTheMountain',
+  googleLink: 'https://goo.gl/maps/rEMTU36ycxbfUxAS6',
   caseName: '大學墅',
 
   houseInfos: [
-    ['投資興建', '佳上佳地產'],
-    ['廣告代銷', '上揚國際建築團隊'],
-    ['設計團隊', '原東聯合建築師事務所'],
-    ['營造公司', '豐禾營建'],
-    ['基地面積', '1186坪'],
-    ['戶數規劃', '59戶'],
+    ['投資興建', '弘華機構'],
+    ['投資興建', '和彩建設有限公司'],
+    ['設計團隊', '意石設計有限公司'],
+    ['建築設計', 'PGA周簡建築師事務所'],
+    ['建照案號', '106峽建字第00212']
   ],
 
-  gtmCode: ['M23CM6L'], // 可放置多個
+  gtmCode: ['52FWZX7'], // 可放置多個
   order: {
     title: '預約賞屋',
     subTitle: ''
