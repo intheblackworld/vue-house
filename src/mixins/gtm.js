84562eb02b5978d080b2209e7b43c430d2d96813
Zuray David <intheblackworld@gmail.com>
2019-09-10 02:42:11 +0800

    replace img and add script tag

--- src/mixins/gtm.js
+++ src/mixins/gtm.js
@@ -21,5 +21,52 @@ export default {
       document.head.appendChild(script.s)
       this.$refs.gtmNoScript.appendChild(script.n)
     })
+    // dss custom
+    // 1. ga new
+    const tag1 = document.createElement('script')
+    tag1.type = 'text/javascript'
+    tag1.src = 'https://www.googletagmanager.com/gtag/js?id=UA-147268639-1'
+    tag1.async = true
+    tag1.innerHTML = `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'UA-147268639-1');
+  `
+    document.head.appendChild(tag1)
+
+    // 2. fb pixel
+    const fbTag = document.createElement('script')
+    fbTag.innerHTML = `!function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2321433564797478');
+    fbq('track', 'PageView');
+  `
+    const fbNoTag = document.createElement('noscript')
+    fbNoTag.innerHTML = `<img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=2321433564797478&ev=PageView&noscript=1" />`
+    document.head.appendChild(fbTag)
+    document.head.appendChild(fbNoTag)
+
+    // 3.  tag3
+    const tag3 = document.createElement('script')
+    tag3.async = true
+    tag3.src = 'https://www.googletagmanager.com/gtag/js?id=AW-741945426'
+    const tag4 = document.createElement('script')
+    tag4.innerHTML = `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'AW-741945426');
+  `
+
+    document.head.appendChild(tag3)
+    document.head.appendChild(tag4)
   }
 }
