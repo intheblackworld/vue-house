efdc34b93d834c5356a2ecb2f65a1bc074e00d60
Zuray David <intheblackworld@gmail.com>
2019-09-10 02:05:12 +0800

    modify navlist

--- src/info/navList.js
+++ src/info/navList.js
@@ -1,10 +1,7 @@
 export default [
-  { name: '周邊環境', imgSrc: '', subTitle: '', section: 'section2', svgSrc: '' },
-  { name: '依山面水', imgSrc: '', subTitle: '', section: 'section3', svgSrc: '' },
-  { name: '與山對話', imgSrc: '', subTitle: '', section: 'section4', svgSrc: '' },
-  { name: '用心深根', imgSrc: '', subTitle: '', section: 'section5', svgSrc: '' },
-  { name: '亮麗的家', imgSrc: '', subTitle: '', section: 'section6', svgSrc: '' },
-  { name: '時尚美學', imgSrc: '', subTitle: '', section: 'section7', svgSrc: '' },
-  { name: '三峽靜好', imgSrc: '', subTitle: '', section: 'section8', svgSrc: '' },
-  { name: '預約賞屋', imgSrc: '', subTitle: '', section: 'contact', svgSrc: '' },
+  { name: '東區首席', imgSrc: '', subTitle: '', section: 'section2', svgSrc: '' },
+  { name: '公園樹海', imgSrc: '', subTitle: '', section: 'section3', svgSrc: '' },
+  { name: '壯闊造鎮', imgSrc: '', subTitle: '', section: 'section4', svgSrc: '' },
+  { name: '菁英團隊', imgSrc: '', subTitle: '', section: 'section5', svgSrc: '' },
+  { name: '優質建材', imgSrc: '', subTitle: '', section: 'section6', svgSrc: '' },
 ]
