b22f10530f1d8cee3d0766f99dcb143f49a54627
Zuray David <intheblackworld@gmail.com>
2019-09-10 20:11:16 +0800

    add info

--- src/info/index.js
+++ src/info/index.js
@@ -1,24 +1,26 @@
 export default {
-  address: '221新北市汐止區康寧街457號',
+  address: '241新北市三重區中正北路大仁街口',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.9827148898416!2d121.62923131549852!3d25.068574983956196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x345d534c7e98316d%3A0xa614b98d1cda13f8!2zMjIx5paw5YyX5biC5rGQ5q2i5Y2A5bq35a-n6KGXNDU36Jmf!5e0!3m2!1szh-TW!2stw!4v1562048804690!5m2!1szh-TW!2stw',
-  phone: '02-2692-1155',
-  fbLink: 'https://www.facebook.com/%E6%B1%90%E6%AD%A2%E6%9D%B1%E5%9F%8E%E5%A4%A7%E5%A2%83-584895791902170/',
-  fbMessage: 'https://m.me/584895791902170',
-  googleLink: 'https://goo.gl/maps/cK2KFFP5boSLGX338',
-  caseName: '東城大境',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.1546910795655!2d121.48948311500689!3d25.0627455839589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a904a97af995%3A0xe8c0cc3a227a6258!2z56-J56a-5Lqk6Z-_6Zmi!5e0!3m2!1szh-TW!2stw!4v1568116832178!5m2!1szh-TW!2stw',
+  phone: '02-2972-8888',
+  fbLink:
+    'https://www.facebook.com/%E7%AF%89%E7%A6%BE%E4%BA%A4%E9%9F%BF%E9%99%A2-694156361100384/?ref=bookmarks',
+  fbMessage: 'https://m.me/694156361100384',
+  googleLink: 'https://goo.gl/maps/k8f7FmHFPEcXkKih6',
+  caseName: '築禾交響院',
 
   houseInfos: [
-    ['投資興建', '合康建設股份有限公司'],
-    ['營造公司', '洛城營造'],
-    ['建築設計', '閤康聯合建築師事務所'],
-    ['景觀設計', '六國景觀-蘇瑞泉大師'],
-    ['公設設計', '研舍設計-林建宏'],
-    ['企劃銷售', '新高創廣告有限公司'],
-    ['接待中心', '新北市汐止區康寧街457號'],
+    ['投資興建', '築禾建設股份有限公司'],
+    ['廣告代銷', '新誠家廣告有限公司'],
+    ['建築設計', '吳成榮建築師事務所'],
+    ['公設設計', '磐石設計有限公司'],
+    ['景觀設計', '老圃造園工程股份有限公司'],
+    ['基地面積', '3,361坪'],
+    ['樓層規劃', '27F/B5'],
+    ['坪數規劃', '2房(30~31坪) 、3房(42~46坪) 、3+1房(48坪)、4房(59~73坪)']
   ],
 
-  gtmCode: ['K8PX6LK'], // 可放置多個
+  gtmCode: ['WBXSH54'], // 可放置多個
   order: {
     title: '預約賞屋',
     subTitle: ''
