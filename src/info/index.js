155990a06d1629b95ea430e85de745c44bdde010
Zuray David <intheblackworld@gmail.com>
2019-09-12 18:27:12 +0800

    add fb

--- src/info/index.js
+++ src/info/index.js
@@ -3,8 +3,8 @@ export default {
   googleSrc:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3611.890636533308!2d121.49905731500833!3d25.139387983923104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ae4f0aa400b1%3A0x75ca2b2ce485be53!2zMTEy5Y-w5YyX5biC5YyX5oqV5Y2A5Lit5ZKM6KGXMjE5LTLomZ8!5e0!3m2!1szh-TW!2stw!4v1567566259616!5m2!1szh-TW!2stw',
   phone: '02-28960111',
-  fbLink: '',
-  fbMessage: '',
+  fbLink: 'https://www.facebook.com/公園大院-117220896322983/',
+  fbMessage: 'https://m.me/117220896322983',
   googleLink: 'https://goo.gl/maps/xecuraRkoCuUYizeA',
   caseName: '公園大院',
 
