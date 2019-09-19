56ee03b78c0d8fb386690bdb7b11e23261f2f0b3
Zuray David <intheblackworld@gmail.com>
2019-09-19 15:06:04 +0800

    modify

--- src/projects/jh/Section8.vue
+++ src/projects/jh/Section8.vue
@@ -401,14 +401,14 @@
   .slide {
     .name {
       position: absolute;
-      left: 0;
       right: 0;
       margin: 0 auto;
-      top: 10px;
+      bottom: 0;
       font-size: 17px;
       color: #fff;
-      text-shadow: 0 1px 2px #000;
       letter-spacing: 2px;
+      background: rgba(0, 0, 0, 0.6);
+      padding: 5px 15px;
     }
   }
 }
