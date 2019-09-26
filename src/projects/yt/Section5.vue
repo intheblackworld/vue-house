f513b026a445ac1accd1a26eff2a8d39fc901af9
Zuray David <intheblackworld@gmail.com>
2019-09-26 20:24:01 +0800

    modify

--- src/projects/yt/Section5.vue
+++ src/projects/yt/Section5.vue
@@ -1,25 +1,17 @@
 <template>
-  <div class="section5">
-    <div class="bg fullscreen">
-      <div class="title-block">
-        <h3 class="title">世紀繁華</h3>
-        <div class="subtitle">Andante！Listen The Greatest World</div>
+  <div class="section4">
+    <div class="bg">
+      <div v-if="!isMobile">
+        <img src="./s5/1.png" alt="耘耕美築 恆久淳萃 公園綠景第一排" class="bg-img" />
+        <img src="./s5/2.png" alt="耘耕美築 恆久淳萃 公園綠景第一排" class="bg-img" />
+        <img src="./s5/logo.png" alt="耘耕美築 恆久淳萃 公園綠景第一排" class="bg-img" />
+        <img src="./s5/t1.png" alt="聽見 森林公園第一排" class="bg-img" />
+        <img src="./s5/t2.png" alt="【下樓 走進一座綠色花園】" class="bg-img" />
+        <img src="./s5/t3.png" alt="【散步 遇見輕軌的美好距離】" class="bg-img" />
+        <img src="./s5/t4.png" alt="【轉彎 公司田溪腳踏車自由行】" class="bg-img" />
       </div>
-      <div class="relative">
-        <div :class="`slide relative`">
-          <img
-            v-for="(slide, index) in slideList"
-            :class="`slide-img ${slideIndex === index ? 'active' : ''}`"
-            :key="`s5-slide-${index}`"
-            :src="slide.src"
-            alt
-          />
-        </div>
-        <div class="text">
-          <h3 class="title">{{slideList[slideIndex].title}}</h3>
-          <div class="subtitle">{{slideList[slideIndex].subtitle}}</div>
-          <div class="desc" v-html="slideList[slideIndex].desc"></div>
-        </div>
+      <div v-else>
+        
       </div>
     </div>
   </div>
@@ -27,60 +19,24 @@
 
 <style lang="scss" scoped>
 .bg {
+  background: url('./s5/bg.jpg');
+  background-size: cover;
   position: relative;
   overflow: hidden;
   position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
 }
 
-.title-block {
-  margin-bottom: 0;
-}
-
-.slide {
-  width: calc(100vw * (1258 / 1920));
-  margin-bottom: 40px;
-}
-
-.text {
+.bg-img {
+  width: 100vw;
   position: absolute;
-  left: 50vw;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 528px;
-  height: 391px;
-  background: linear-gradient(
-    to right,
-    rgba(230, 176, 72, 0.7) 0%,
-    rgba(230, 176, 72, 1) 30%
-  );
-  padding: 80px 50px;
+  top: 0;
+  left: 0;
+  height: auto;
+  display: block;
+  object-fit: cover;
 
-  .title {
-    font-size: 40px;
-    font-weight: bold;
-    text-align: left;
-    color: #312000;
-  }
-
-  .subtitle {
-    font-family: Playball;
-    font-size: 27px;
-    line-height: 2;
-    text-align: left;
-    color: #312000;
-  }
-
-  .desc {
-    font-size: 18px;
-    font-weight: 300;
-    line-height: 1.67;
-    letter-spacing: 1.8px;
-    text-align: justify;
-    color: #000000;
+  &:nth-child(1) {
+    position: relative;
   }
 }
 
@@ -95,59 +51,12 @@
 @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
   .bg {
   }
-
-  .text {
-    width: 508px;
-    height: 270px;
-    padding: 20px 40px;
-  }
 }
 
 @media screen and (max-width: 767px) {
   .bg {
-  }
-
-  .fullscreen {
-    height: auto !important;
-  }
-
-  .slide {
-    width: 100vw;
-  }
-
-  .text {
-    position: relative;
-    left: auto;
-    top: auto;
-    margin: 0 auto;
-    transform: translateY(-30px);
-    width: 90vw;
-    height: 260px;
-    padding: 40px 30px;
-
-    .title {
-      font-size: 27px;
-      font-weight: bold;
-      text-align: left;
-      color: #312000;
-    }
-
-    .subtitle {
-      font-family: Playball;
-      font-size: 18px;
-      line-height: 2;
-      text-align: left;
-      color: #312000;
-    }
-
-    .desc {
-      font-size: 12px;
-      font-weight: 300;
-      line-height: 1.67;
-      letter-spacing: 1.7px;
-      text-align: justify;
-      color: #000000;
-    }
+    background: url('./s5/mo/bg.jpg');
+    height: 100%;
   }
 }
 </style>
@@ -155,40 +64,16 @@
 <script>
 // @ is an alias to /src
 import { isMobile } from '@/utils'
-import slider from '@/mixins/slider.js'
-import { setInterval } from 'timers'
 
 export default {
-  name: 'section5',
-  mixins: [slider],
+  name: 'section4',
 
   data() {
     return {
       isMobile,
-      slideList: [
-        {
-          src: require('./s5/1.jpg'),
-          title: '橫空出世',
-          subtitle: 'Romanesque Style',
-          desc:
-            '4座羅馬神殿式拱穹＋1座富麗輝煌的圓形穹頂<br />如同偉大作家歌德所說的<br />騰空而起，像崇高壯觀、濃蔭廣覆的建築之樹',
-        },
-        {
-          src: require('./s5/2.jpg'),
-          title: '向榮冠冕的萬丈城市力',
-          subtitle: 'Crown City',
-          desc:
-            '高仰度28F摩天視界、4棟口字型高樓層地標式建築，可眺望台北城市無敵豪景，北美館、圓山飯店、台北101…爭藏地標、盡收眼底，壯麗開卷般的生活尺度，成就景觀無敵的眼界奢華，更能放心追逐您心之所向的視界巔峰。',
-        },
-      ],
     }
   },
-  methods: {},
 
-  created() {
-    setInterval(() => {
-      this.addIndex()
-    }, 5000)
-  },
+  methods: {},
 }
 </script>
