c73ed6ca7f9c3f991b38b4a3e9e8468fa8e84865
Zuray David <intheblackworld@gmail.com>
2019-10-04 16:18:03 +0800

    modify

--- src/info/index.js
+++ src/info/index.js
@@ -4,8 +4,8 @@ export default {
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.2828477979488!2d121.43031001500945!3d25.19368218389782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442bab046ca698f%3A0x77ff2619010dfa77!2zMjUx5paw5YyX5biC5reh5rC05Y2A5paw5biC5LqM6Lev5LiJ5q61MTbomZ8!5e0!3m2!1szh-TW!2stw!4v1569582560689!5m2!1szh-TW!2stw',
   phone: '02-2623-0055',
   fbLink:
-    '',
-  fbMessage: '',
+    'https://www.facebook.com/頤昌耘萃-1911708729149992/ ',
+  fbMessage: 'https://m.me/1911708729149992/',
   googleLink: 'https://goo.gl/maps/igUJQcvUNurQ5Csy8',
   caseName: '耘萃',
 
