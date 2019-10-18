51d7b9ad49ecdc2ed2ca7b65941e6aa31b13d6b0
Zuray David <intheblackworld@gmail.com>
2019-10-18 18:01:20 +0800

    modify

--- src/info/index.js
+++ src/info/index.js
@@ -3,8 +3,7 @@ export default {
   googleSrc:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.2828477979488!2d121.43031001500945!3d25.19368218389782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442bab046ca698f%3A0x77ff2619010dfa77!2zMjUx5paw5YyX5biC5reh5rC05Y2A5paw5biC5LqM6Lev5LiJ5q61MTbomZ8!5e0!3m2!1szh-TW!2stw!4v1569582560689!5m2!1szh-TW!2stw',
   phone: '02-2623-0055',
-  fbLink:
-    'https://www.facebook.com/頤昌耘萃-1911708729149992/ ',
+  fbLink: 'https://www.facebook.com/頤昌耘萃-1911708729149992/ ',
   fbMessage: 'https://m.me/1911708729149992/',
   googleLink: 'https://goo.gl/maps/igUJQcvUNurQ5Csy8',
   caseName: '耘萃',
@@ -13,7 +12,9 @@ export default {
     ['投資興建', '頤昌建設股份有限公司'],
     ['建築規劃', '陳朝雄建築師事務所'],
     ['建照號碼', '107淡建字第00097號'],
-    ['基地地址', '新北市淡水區濱海路二段202巷(196地號)']
+    ['基地地址', '新北市淡水區濱海路二段202巷(196地號)'],
+    ['廣告銷售', '鈞驛行銷團隊'],
+    ['廣告企劃', '天彤廣告']
   ],
 
   gtmCode: ['MFTZQL4'], // 可放置多個
