05568d3232097eca0d1c5335c3e59f456f87ae5c
xcatx7 <xcatx796@gmail.com>
2020-01-09 19:14:36 +0800

    換我們的logo跟+連結

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="footer">
-    <img src="../assets/img/footerLogo.png" alt="心天畝的圖片" />
-    <a href=" https://www.h35.tw/admin/test/login.php" target="_blank">網頁製作</a>
+    <a href="https://www.lixin.com.tw/" target="_blank"><img src="../assets/img/footerLogo.png" alt="立炘數位" /></a>
+    <a href="https://www.h35.tw/admin/test/login.php" target="_blank">網頁製作</a>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -13,35 +13,39 @@
   display: flex;
   align-items: center;
   justify-content: center;
+  line-height: 1.4;
+  font-size: 12px;
+  vertical-align: middle;
 
   img {
-    width: 50px;
+    height:1.4em;
     margin-right: 20px;
+    vertical-align: inherit;
   }
 
   p {
-    font-size: 12px;
     color: rgba(255, 255, 255, 0.6);
     margin-right: 15px;
     margin-bottom: 0;
+    vertical-align: inherit;
   }
 
   a {
-    font-size: 12px;
     color: #fff;
     text-decoration: none;
+    vertical-align: inherit;
   }
 }
 
 @media screen and (max-width: 767px) {
   .footer {
-    margin-bottom: 0px;
+    margin-bottom:63px;
   }
 }
 
 @media only screen and (max-width: 767px) {
   .footer {
-    margin-bottom: 0px;
+    margin-bottom: 63px;
   }
 }
 </style>
