97d4587aad55e41977680b24cd6207933d04ae59
Zuray David <intheblackworld@gmail.com>
2020-02-20 15:12:13 +0800

    改成新版contact-form

--- contact-form.php
+++ contact-form.php
@@ -1,4 +1,57 @@
 <?php
+    $case_name = "輕井澤";
+    $case_code = 'cjt';
+
+    $name         = isset($_POST['name']) ? $_POST['name'] : '';
+    $phone        = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $user_email   = isset($_POST['email']) ? $_POST['email'] : '';
+    $city         = isset($_POST['city']) ? $_POST['city'] : '';
+    $area         = isset($_POST['area']) ? $_POST['area'] : '';
+    $msg          = isset($_POST['msg']) ? $_POST['msg'] : '';
+    $utm_source   = isset($_POST['utm_source']) ? $_POST['utm_source'] : '';
+    $utm_medium   = isset($_POST['utm_medium']) ? $_POST['utm_medium'] : '';
+    $utm_content  = isset($_POST['utm_content']) ? $_POST['utm_content'] : '';
+    $utm_campaign = isset($_POST['utm_campaign']) ? $_POST['utm_campaign'] : '';
+    $datetime     = date ("Y-m-d H:i:s" , mktime(date('H'), date('i'), date('s'), date('m'), date('d'), date('Y'))) ;
+
+    # 不同版本前端相容 Start
+    if ($name == '') {
+        $name = isset($_POST['widget-contact-form-name']) ? $_POST['widget-contact-form-name'] : '';
+    }
+    if ($phone == '') {
+        $phone = isset($_POST['widget-contact-form-phone']) ? $_POST['widget-contact-form-phone'] : '';
+    }
+    if ($user_email == '') {
+        $user_email = isset($_POST['widget-contact-form-email']) ? $_POST['widget-contact-form-email'] : '';
+    }
+    if ($city == '') {
+        $city = isset($_POST['widget-contact-form-city']) ? $_POST['widget-contact-form-city'] : '';
+    }
+    if ($area == '') {
+        $area = isset($_POST['widget-contact-form-area']) ? $_POST['widget-contact-form-area'] : '';
+    }
+    if ($msg == '') {
+        $msg = isset($_POST['widget-contact-form-msg']) ? $_POST['widget-contact-form-msg'] : '';
+    }
+    if ($utm_source == '') {
+        $utm_source = isset($_POST['widget-contact-form-utm_source']) ? $_POST['widget-contact-form-utm_source'] : '';
+    }
+    if ($utm_source == '') {
+        $utm_source = isset($_POST['widget-contact-form-utm_source']) ? $_POST['widget-contact-form-utm_source'] : '';
+    }
+    if ($utm_medium == '') {
+        $utm_medium = isset($_POST['widget-contact-form-utm_medium']) ? $_POST['widget-contact-form-utm_medium'] : '';
+    }
+    if ($utm_content == '') {
+        $utm_content = isset($_POST['widget-contact-form-utm_content']) ? $_POST['widget-contact-form-utm_content'] : '';
+    }
+    if ($utm_campaign == '') {
+        $utm_campaign = isset($_POST['widget-contact-form-utm_campaign']) ? $_POST['widget-contact-form-utm_campaign'] : '';
+    }
+
+    $datetime = date ("Y-m-d H:i:s" , mktime(date('H'), date('i'), date('s'), date('m'), date('d'), date('Y'))) ;
+    # 不同版本前端相容 End
+
     # PDO DB 連線 Start
     $pdo=new pdo('mysql:host=localhost;dbname=htw_web','htw','748aSgl5Ni');
     $pdo->exec("SET NAMES 'utf8'");
@@ -19,19 +72,17 @@
     # 檢查是否時間範圍內重複留言 Start
     $sCheckMsg = ''; //檢查留言內容
 
-    if ($_POST['msg'] == '') {
-        $_POST['msg'] = '無留言';
+    if ($msg == '') {
+        $msg = '無留言';
     }
 
-    if (($_COOKIE['msg'] != null) && ($_POST['msg'] != null)) {
+    if ($_COOKIE['msg'] != null) {
         $sCheckMsg = $_COOKIE['msg'];
-        setcookie ("msg", $_POST['msg'], time()+36400);
-    } else {
-        setcookie ("msg", $_POST['msg'], time()+36400);
     }
+    setcookie ("msg", $msg, time()+36400);
 
 
-    if (($sCheckMsg === $_POST['msg'])) { //要三個等號, 不然 null 會等於 empty
+    if (($sCheckMsg === $msg)) { //要三個等號, 不然 null 會等於 empty
         $bCheck = false;
     }
     # 檢查是否時間範圍內重複留言 End
@@ -76,7 +127,7 @@
     // $aFilterKeyWordList = array('game', 'casino', '測試', 'test');
     $checkConunt = 0;
     foreach ($aFilterKeyWordList as $key => $sFilterKeyWord) {
-        $checkConunt = explode($sFilterKeyWord, $_POST['msg']);
+        $checkConunt = explode($sFilterKeyWord, $msg);
         if (count($checkConunt) > 1) {
             $bCheck = false;
         }
@@ -87,7 +138,7 @@
     // $aFilterNameList = array('蔡銘聰');
     $checkConunt = 0;
     foreach ($aFilterNameList as $key => $sFilterName) {
-        $checkConunt = explode($sFilterName, $_POST['name']);
+        $checkConunt = explode($sFilterName, $name);
         if (count($checkConunt) > 1) {
             $bCheck = false;
         }
@@ -97,7 +148,7 @@
     # 檢查信箱 Start
     $checkConunt = 0;
     foreach ($aFilterEmailList as $key => $aFilterValue) {
-        $checkConunt = explode($aFilterValue, $_POST['email']);
+        $checkConunt = explode($aFilterValue, $user_email);
         if (count($checkConunt) > 1) {
             $bCheck = false;
         }
@@ -107,7 +158,7 @@
     # 檢查電話 Start
     $checkConunt = 0;
     foreach ($aFilterPhoneList as $key => $aFilterValue) {
-        $checkConunt = explode($aFilterValue, $_POST['phone']);
+        $checkConunt = explode($aFilterValue, $phone);
         if (count($checkConunt) > 1) {
             $bCheck = false;
         }
@@ -124,6 +175,15 @@
     }
     # 檢查IP End
 
+    # 取得信箱帳號密碼, Insert google API token Start
+    $sql = "SELECT * FROM `mail_token` limit 0,1";
+    $aGetMailTokenDbList = $pdo->query($sql)->fetchAll();
+    $token    = $aGetMailTokenDbList[0]['token'];
+    $mailName = $aGetMailTokenDbList[0]['mail'];
+    $mailPwd  = $aGetMailTokenDbList[0]['pwd'];
+    # 取得信箱帳號密碼, Insert google API token End
+
+    # 老版本讀取 Start
     $db_host = 'localhost';
     $db_user = 'htw';
     $db_pass = '748aSgl5Ni';
@@ -133,7 +193,7 @@
     mysql_query("SET NAMES UTF8");
     mysql_select_db($db_name, $con);
 
-    $query = "SELECT tomail FROM susers WHERE email = 'cjt'";
+    $query = "SELECT tomail FROM susers WHERE email = '".$case_code."'";
     $result = mysql_query($query, $con);
     $row = mysql_fetch_row($result);
 
@@ -150,30 +210,11 @@
     {
         $tomail_admin = $row_admin[0];
     }
-?>
-<!DOCTYPE html>
-<html lang="zh-tw">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-</head>
-<body>
-<?php
+    # 老版本讀取 End
+
     ini_set("display_errors", "On");
     date_default_timezone_set("Asia/Taipei");
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $user_email = $_POST['email'];
-    $city = $_POST['city'];
-    $area = $_POST['area'];
-    $msg = $_POST['msg'];
-    $datetime = date ("Y-m-d H:i:s" , mktime(date('H'), date('i'), date('s'), date('m'), date('d'), date('Y'))) ;
-    $utm_source = $_POST['utm_source'];
-    $utm_medium = $_POST['utm_medium'];
-    $utm_content = $_POST['utm_content'];
-    $utm_campaign = $_POST['utm_campaign'];
-
-    $case_name = "輕井澤";
-
+    
     require_once('php-mailer/PHPMailerAutoload.php'); //引入phpMailer 記得將路徑換成您自己的path
 
     $mail= new PHPMailer(); //建立新物件
@@ -183,14 +224,14 @@
     $mail->Port = 25; //設定SMTP埠位，預設為25埠。
     $mail->CharSet = "utf-8"; //設定郵件編碼
 
-    $mail->Username = "service@h35.tw"; //設定驗證帳號
-    $mail->Password = "xu4vupAZ"; //設定驗證密碼
+    $mail->Username = $mailName; //設定驗證帳號
+    $mail->Password = $mailPwd; //設定驗證密碼
 
     $mail->From = "noreply@h35.tw"; //設定寄件者信箱
     $mail->FromName = $case_name." - 官網網站"; //設定寄件者姓名
 
     $mail->Subject = $case_name." - 官網網站"; //設定郵件標題
-    $mail->Body = "網站：https://cjt.h35.tw/<BR>姓名：".$name."<BR>電話：".$phone."<BR>信箱：".$user_email."<BR>城市：".$city.$area."<BR>留言：".$msg."<BR><BR>填表日期：".$datetime."<BR>廣告來源：".$utm_source."<BR>廣告媒介：".$utm_medium."<BR>廣告名稱：".$utm_campaign."<BR>廣告內容：".$utm_content; //設定郵件內容
+    $mail->Body = "網站：https://".$case_code.".h35.tw/<BR>姓名：".$name."<BR>電話：".$phone."<BR>信箱：".$user_email."<BR>城市：".$city.$area."<BR>留言：".$msg."<BR><BR>填表日期：".$datetime."<BR>廣告來源：".$utm_source."<BR>廣告媒介：".$utm_medium."<BR>廣告名稱：".$utm_campaign."<BR>廣告內容：".$utm_content; //設定郵件內容
     $mail->IsHTML(true); //設定郵件內容為HTML
 
     $tomail_arr = explode(",",$tomail);
@@ -198,6 +239,39 @@
 
     //檢查沒問題才寄出信件
     if ($bCheck == true) { //if start
+
+	    # 添加到 Googlde 資料DB Start
+        try {
+            $url = "http://35.221.147.147/send.php";
+            $url .= "?token=".$token;
+            $url .= "&name=".$name;
+            $url .= "&phone=".$phone;
+            $url .= "&email=".$user_email;
+            $url .= "&city=".$city;
+            $url .= "&area=".$area;
+            $url .= "&message=".$msg;
+            $url .= "&utm_source=".$utm_source;
+            $url .= "&utm_medium=".$utm_medium;
+            $url .= "&utm_content=".$utm_content;
+            $url .= "&utm_campaign=".$utm_campaign;
+            $url .= "&case_code=".$case_code;
+            $url .= "&reservation_datetime=".$datetime;
+
+            $ch = curl_init();
+            curl_setopt($ch,CURLOPT_URL,$url);
+            curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+            curl_setopt($ch,CURLOPT_TIMEOUT,1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            // echo "send ok";
+
+        } catch (Exception $e) {
+
+            // echo "send fail";
+        }
+        # 添加到 Googlde 資料DB End
+
         foreach($tomail_arr as $email)
         {
            $mail->AddAddress("$email");
@@ -216,6 +290,12 @@
     } //if end
 
 ?>
+<!DOCTYPE html>
+<html lang="zh-tw">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+</head>
+<body>
 <script src="js/jquery.js"></script>
 
 <script type="text/javascript">
