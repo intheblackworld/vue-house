51625d93e07955fdd218b28c129de616456640f0
Zuray David <intheblackworld@gmail.com>
2020-05-11 19:36:45 +0800

    modify

--- src/info/navList.js
+++ src/info/navList.js
@@ -3,7 +3,7 @@ export default [
   { name: '幸福圓', imgSrc: '', subTitle: '', section: 'section4', svgSrc: '' },
   { name: '五感建築', imgSrc: '', subTitle: '', section: 'section7', svgSrc: '' },
   { name: '精選建材', imgSrc: '', subTitle: '', section: 'section8', svgSrc: '' },
-  { name: '幸福金三角', imgSrc: '', subTitle: '', section: 'sectio11', svgSrc: '' },
+  { name: '幸福金三角', imgSrc: '', subTitle: '', section: 'section11', svgSrc: '' },
   { name: '菁英團隊', imgSrc: '', subTitle: '', section: 'section12', svgSrc: '' },
   { name: '預約賞屋', imgSrc: '', subTitle: '', section: 'contact', svgSrc: '' },
 ]                
