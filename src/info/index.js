07f817fb4f4836241933237b65f6bb1e257734e6
xcatx7 <xcatx796@gmail.com>
2020-06-01 11:58:06 +0800

    cjl 表單修正

--- src/info/index.js
+++ src/info/index.js
@@ -1,5 +1,5 @@
 export default {
-  address: '台南市東區崇吉六街＆崇元二街',
+  address: '台南市東區生產路&崇明路口',
   googleSrc:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7347.2397624275245!2d120.221094835133!3d22.964222199061425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjLCsDU3JzUxLjIiTiAxMjDCsDEzJzMxLjciRQ!5e0!3m2!1szh-TW!2stw!4v1590729662083!5m2!1szh-TW!2stw',
   googleLink: 'https://goo.gl/maps/mxvGn8wayCpnDRPt5',
