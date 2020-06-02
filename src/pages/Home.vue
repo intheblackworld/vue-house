99755227ae2d7dceb1e86b7e215f49fdb766f5aa
Zuray David <intheblackworld@gmail.com>
2020-06-02 10:07:02 +0800

    modify

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -166,7 +166,7 @@ export default {
       for (let n = 0; n < offsetTopArr.length; n++) {
         // 如果 scrollTop 大于等于第n个元素的 offsetTop 则说明 n-1 的内容已经完全不可见
         // 那么此时导航索引就应该是n了
-        if (scrollTop >= offsetTopArr[n] - 100) {
+        if (scrollTop >= offsetTopArr[n] - 200) {
           navIndex = n
         }
       }
