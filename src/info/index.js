98f9276d335fff24f5fe6aa560cd2c45fd016239
xcatx7 <xcatx796@gmail.com>
2020-06-08 10:25:41 +0800

    yu 修正

--- src/info/index.js
+++ src/info/index.js
@@ -1,23 +1,21 @@
 export default {
-  address: '桃園市蘆竹區大興路271號',
+  address: '台北市信義區永吉路278巷',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7230.9960651246065!2d121.25284363490806!3d25.017167214282612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3468202b8c0d6a65%3A0xbeba6a66370564d3!2zMzM45qGD5ZyS5biC6JiG56u55Y2A5aSn6IiI6LevMjcx6Jmf!5e0!3m2!1szh-TW!2stw!4v1590377353061!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/Bn6zU3ZA32NUT7oY7',
-  phone: '03-3135533',
-  fbLink: 'https://www.facebook.com/豐郡大景-108654404191724',
-  fbMessage: 'https://m.me/108654404191724',
-  caseName: '豐郡大景',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.707213659351!2d121.57194771500643!3d25.04400838396768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442aba2603c8119%3A0xcd80278bca1cd936!2zMTEw5Y-w5YyX5biC5L-h576p5Y2A5rC45ZCJ6LevMjc45be3!5e0!3m2!1szh-TW!2stw!4v1591582711907!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/GChDgBBxSccG7xCN7',
+  phone: '02-2760-5888',
+  fbLink: 'https://www.facebook.com/',
+  fbMessage: 'https://m.me/',
+  caseName: '展宜詠喆',
   indigatorLength: 7,
 
   houseInfos: [
-    ['投資興建', '豐郡建設股份有限公司'],
-    ['責任施工', '當代營造有限公司'],
-    ['建築規劃', '林永發建築師事務所'],
-    ['結構設計', '林永發建築師事務所'],
-    ['門廳設計', '垣樣規劃設計工程有限公司'],
-    ['企劃銷售', '鈞驛廣告股份有限公司'],
-    ['廣告企劃', '天彤廣告'],
-    ['建照號碼', '(103)桃縣工建執照字第會蘆00779-02號'],
+    ['投資興建', '展宜建築開發股份有限公司'],
+    ['建築設計', '高鴻文建築師事務所'],
+    ['公共空間', '創研空間設計'],
+    ['建築代銷', '聯碩行銷股份有限公司'],
+    ['建案位置', '台北市信義區永吉路278巷'],
+    ['建照號碼', '108建字第0245號'],
   ],
 
   gtmCode: ['K97QDJM'], // 可放置多個
