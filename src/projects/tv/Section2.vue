d5a8227666ec039b5a86ae07a19ebaf91cd62408
xcatx7 <xcatx796@gmail.com>
2020-06-18 21:19:33 +0800

    tv改圖

--- src/projects/tv/Section2.vue
+++ src/projects/tv/Section2.vue
@@ -131,7 +131,7 @@
         <div
           @click="setIndex(index)"
           :class="`indigator ${slideList[slideIndex].contentIndex === index ? 'active' : ''}`"
-          v-for="index in [0, 1, 2]"
+          v-for="index in [0, 1, 2, 3, 4, 5]"
           :key="`indigator-1-${index}`"
         ></div>
       </div>
