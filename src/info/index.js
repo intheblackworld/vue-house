f544c75b6db068404df5b323ef7f5e1c1ceee6c1
xcatx <xcatx796@gmail.com>
2020-07-08 18:21:51 +0800

    央北鑫建築 換資訊

--- src/info/index.js
+++ src/info/index.js
@@ -4,13 +4,13 @@ export default {
   address: '新北市五股區新城五路與新城八路交叉',
   // isMobile ? '接待中心：<br />106台北市大安區基隆路三段20-1號' : '接待中心：106台北市大安區基隆路三段20-1號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.293723920597!2d121.44400571738508!3d25.09191669899662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDA1JzMwLjkiTiAxMjHCsDI2JzQ2LjMiRQ!5e0!3m2!1szh-TW!2stw!4v1590128993509!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/wUFoxM69s4RmGV6a6',
-  phone: '02-82951777',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3616.6615014279905!2d121.5284838154263!3d24.977628883998797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3468021c99e12edb%3A0x2bd39f5ee4c8d9cd!2zMjMx5paw5YyX5biC5paw5bqX5Y2A5pav6aao5LiA6Lev!5e0!3m2!1szh-TW!2stw!4v1594203653578!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/UfyLX5Rm7sxoe1XZ8',
+  phone: '02-86676688',
   fbLink:
-    'https://www.facebook.com/神采飛洋-101802774887170',
-  fbMessage: 'https://m.me/101802774887170',
-  caseName: '神采飛洋',
+    'https://www.facebook.com/央北鑫建築-107215314395480/',
+  fbMessage: 'https://m.me/107215314395480/',
+  caseName: '央北鑫建築',
   houseInfos: [
     ['投資興建', '鑫忠建築開發股份有限公司'],
     ['建築規劃', '張維哲建築師事務所'],
@@ -23,7 +23,7 @@ export default {
     ['經紀人', '戴子順 (96)竹縣地經字第00150'],
   ],
 
-  gtmCode: ['PS8ZD97'], // 可放置多個
+  gtmCode: ['TK7BQ55'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
