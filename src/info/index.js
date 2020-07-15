5d7c5976070f399ee58b1c0c19b275e4e03f1085
xcatx <xcatx796@gmail.com>
2020-07-15 11:54:22 +0800

    上東京先換資訊

--- src/info/index.js
+++ src/info/index.js
@@ -1,23 +1,21 @@
 export default {
-  address: '桃園市蘆竹區大興路271號',
+  address: '高雄市三民區明誠一路491號隔壁',
   googleSrc:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7230.9960651246065!2d121.25284363490806!3d25.017167214282612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3468202b8c0d6a65%3A0xbeba6a66370564d3!2zMzM45qGD5ZyS5biC6JiG56u55Y2A5aSn6IiI6LevMjcx6Jmf!5e0!3m2!1szh-TW!2stw!4v1590377353061!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/Bn6zU3ZA32NUT7oY7',
-  phone: '03-3135533',
-  fbLink: 'https://www.facebook.com/豐郡大景-108654404191724',
-  fbMessage: 'https://m.me/108654404191724',
+  googleLink: 'https://maps.app.goo.gl/vxYmSUdgnYF4EEiL6',
+  phone: '07-3925188',
+  fbLink: 'https://www.facebook.com/dalitokyo888/',
+  fbMessage: 'https://m.me/dalitokyo888/',
   caseName: '上東京',
   indigatorLength: 10,
 
   houseInfos: [
-    ['投資興建', '豐郡建設股份有限公司'],
-    ['責任施工', '當代營造有限公司'],
-    ['建築規劃', '林永發建築師事務所'],
-    ['結構設計', '林永發建築師事務所'],
-    ['門廳設計', '垣樣規劃設計工程有限公司'],
-    ['企劃銷售', '鈞驛廣告股份有限公司'],
-    ['廣告企劃', '天彤廣告'],
-    ['建照號碼', '(103)桃縣工建執照字第會蘆00779-02號'],
+    ['投資興建', '達麗建設事業股份有限公司'],
+    ['責任施工', '寶信營造股份有限公司'],
+    ['建築設計', '原東聯合建築師事務所'],
+    ['棟戶規劃', '3棟，320戶住家，4戶店面'],
+    ['基地面積', '1334坪'],
+    ['樓層規劃', '地上24層，地下3層'],
   ],
 
   gtmCode: ['NTKG2VP'], // 可放置多個
