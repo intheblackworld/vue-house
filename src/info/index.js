eedeedc4b3f375778be78c4d86c3e706b6c19a27
xcatx <xcatx796@gmail.com>
2020-07-30 16:40:44 +0800

    sdj

--- src/info/index.js
+++ src/info/index.js
@@ -1,8 +1,8 @@
 export default {
   address: '高雄市三民區明誠一路491號隔壁',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3681.8314561513253!2d120.31828251541931!3d22.660071835463828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e05cbf12b1aad%3A0x36423055d973658!2z6YGU6bqX5LiK5p2x5LqsIOaOpeW-heS4reW_gw!5e0!3m2!1szh-TW!2stw!4v1594785888137!5m2!1szh-TW!2stw',
-  googleLink: 'https://maps.app.goo.gl/vxYmSUdgnYF4EEiL6',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1840.9175873705049!2d120.32020565882075!3d22.65993322638741!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e05cbf12b1aad%3A0x36423055d973658!2z6YGU6bqX5LiK5p2x5LqsIOaOpeW-heS4reW_gw!5e0!3m2!1szh-TW!2stw!4v1596098033334!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/PNJvdSdoPWvW5xVs5',
   phone: '07-3925188',
   fbLink: 'https://www.facebook.com/dalitokyo888/',
   fbMessage: 'https://m.me/dalitokyo888/',
