feee02ecef98fb2f24b200ba4c7b6986a8c3be7c
Zuray David <intheblackworld@gmail.com>
2020-08-24 17:55:22 +0800

    modify

--- src/info/navList.js
+++ src/info/navList.js
@@ -1,5 +1,5 @@
 export default [
-  { name: '小城在地情', imgSrc: '', subTitle: '', section: 'section1', svgSrc: '', offset: -100 },
+  { name: '小城在地情', imgSrc: '', subTitle: '', section: 'section1', svgSrc: '', offset: -80 },
   { name: '樂居小城', imgSrc: '', subTitle: '', section: 'section3', svgSrc: '', offset: -100 },
   { name: '便捷小城', imgSrc: '', subTitle: '', section: 'section5', svgSrc: '', mobileOffset: 0 },
   { name: '風采小城', imgSrc: '', subTitle: '', section: 'section6', svgSrc: '', mobileOffset: 0 },
