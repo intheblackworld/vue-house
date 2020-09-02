825ac6779c448ccaa4ab0026fef352ad458ab6bf
Zuray David <intheblackworld@gmail.com>
2020-09-03 02:53:33 +0800

    modify

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -180,8 +180,6 @@
           :disabled="!checked || !isVerify"
           @click="submit"
           :loading="isSubmit"
-          data-aos="fade-down"
-          data-aos-delay="700"
         >預約賞屋</el-button>
         <Loading
           :loading="isSubmit"
