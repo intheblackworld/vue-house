9a0994a3363fb2151e2a53c787d2a7b4d015b2bd
xcatx <xcatx796@gmail.com>
2020-09-22 14:03:38 +0800

    gtm換

--- src/info/index.js
+++ src/info/index.js
@@ -17,7 +17,7 @@ export default {
     ['建照號碼', '108.07.05建管建見字第289至303號',],
   ],
 
-  gtmCode: ['PMSHFQH'], // 可放置多個
+  gtmCode: ['KN6WXLL'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
