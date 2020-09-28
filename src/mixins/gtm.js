335c363b4f970abdf89579b37755a6bd4c525fbb
xcatx <xcatx796@gmail.com>
2020-09-28 14:03:00 +0800

    預告頁收到1  拿掉gtm給am測試

--- src/mixins/gtm.js
+++ src/mixins/gtm.js
@@ -5,11 +5,11 @@ export default {
       const s = document.createElement('script')
       s.type = 'text/javascript'
       s.async = true
-      s.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-${gtm}');`
+      s.innerHTML =gtm? `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-${gtm}');`:``
       const n = document.createElement('noscript')
       n.type = 'text/javascript'
       n.async = true
-      n.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-${gtm}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+      n.innerHTML = gtm?`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-${gtm}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`:``
       const obj = {
         s,
         n
