74a50f3a01b01a212641d56431873f769319f018
Zuray David <intheblackworld@gmail.com>
2020-09-30 15:03:39 +0800

    modify

---
+++ src/projects/wop/Section1.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="section1">
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import '@/assets/style/function.scss';
+
+.section1 {
+  width: size(1920);
+  height: 100vh;
+  min-height: size(1080);
+  background-size: cover;
+  background-attachment: fixed;
+  overflow: hidden;
+}
+
+@media only screen and (max-width: 1440px) {
+}
+@media only screen and (max-width: 1280px) and (min-width: 1025px) {
+  .fullscreen {
+    height: 100vh;
+  }
+}
+
+/* 螢幕尺寸標準 */
+/* 平板尺寸 */
+@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+}
+
+@media screen and (max-width: 767px) {
+  .section1 {
+    width: 100vw;
+    min-height: auto;
+    height: sizem(604 + 329);
+    //background-image: url('./mo/1/00.jpg');
+    background-size: cover;
+    background-attachment: scroll;
+  }
+}
+</style>
+<script>
+// @ is an alias to /src
+import { isMobile, isTablet } from '@/utils'
+
+export default {
+  name: 'section1',
+
+  data() {
+    return {
+      isMobile,
+      isTablet,
+    }
+  },
+
+  methods: {},
+
+  created() {},
+
+  mounted() {},
+
+  computed: {},
+}
+</script>
