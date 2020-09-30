c953c1520f7da333de934bce6539c82d3d9a32bc
Zuray David <intheblackworld@gmail.com>
2020-09-30 13:46:57 +0800

    modify

--- src/info/index.js
+++ src/info/index.js
@@ -20,7 +20,7 @@ export default {
     // ['坪數規劃', '2房(30~31坪) 、3房(42~46坪) 、3+1房(48坪)、4房(59~73坪)']
   ],
 
-  gtmCode: ['NV2FC34'], // 可放置多個
+  gtmCode: [''], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
