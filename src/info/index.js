082e5fb8774c50d505db7e5055b5bda6d3b7ea4c
xcatx <xcatx796@gmail.com>
2020-10-14 16:15:23 +0800

    cjl1 gtm

--- src/info/index.js
+++ src/info/index.js
@@ -17,7 +17,7 @@ export default {
    // ['使用執照', '108店使字第00294號'],
   ],
 
-  gtmCode: [''], // 可放置多個
+  gtmCode: ['MSDRJWK'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
