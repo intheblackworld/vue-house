9ab0a77c7e71900e01b84ce50449bcf241763adc
Zuray David <intheblackworld@gmail.com>
2020-10-27 20:47:37 +0800

    modify

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -346,7 +346,7 @@ export default {
 }
 .order-bg {
   // background-color: $order_bg_color;
-  // background-image: $order_bg_image;
+  background-image: $order_bg_image;
   background-repeat: no-repeat;
   position: relative;
   padding-top: 130px;
