b1564d6590c41b4990d02bf6ac7321abd8c8fe56
xcatx <xcatx796@gmail.com>
2020-11-24 18:50:51 +0800

    jw  修細節

--- src/info/index.js
+++ src/info/index.js
@@ -5,8 +5,8 @@ export default {
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.5006040482017!2d121.47356631500666!3d25.051016483964386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a8f6eb73de7f%3A0xcd6eec18a60f8730!2zMjQx5paw5YyX5biC5LiJ6YeN5Y2A5LqU6LC3546L5YyX6KGXNDjomZ8!5e0!3m2!1szh-TW!2stw!4v1605628939458!5m2!1szh-TW!2stw',
   googleLink: 'https://goo.gl/maps/1vxT7Dtx5QZctzVP6',
   phone: '02-89856888',
-  fbLink: 'https://www.facebook.com/中山直美-115702496966413',
-  fbMessage: 'https://m.me/115702496966413',
+  fbLink: 'https://www.facebook.com/京旺協奏曲-102351781701612/',
+  fbMessage: 'https://m.me/102351781701612/',
   caseName: '京旺協奏曲',
   indigatorLength: 10,
 
