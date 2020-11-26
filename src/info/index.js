3f624a811b155240781f480a35b35a2739b77a35
xcatx <xcatx796@gmail.com>
2020-11-26 10:13:53 +0800

    gtm修正

--- src/info/index.js
+++ src/info/index.js
@@ -21,7 +21,7 @@ export default {
     ['建照號碼', '109重建字第00491'],
   ],
 
-  gtmCode: ['WX44LWQ'], // 可放置多個
+  gtmCode: ['NVQRL37'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
