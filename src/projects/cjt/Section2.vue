392c0d50f31489a4bc733ab78a93ec9a2b5e04eb
xcatx <xcatx796@gmail.com>
2021-01-04 11:04:07 +0800

    cjt

--- src/projects/cjt/Section2.vue
+++ src/projects/cjt/Section2.vue
@@ -115,13 +115,13 @@
   .bg {
     background-image: url('./s2/mo/bg.jpg');
   }
-.if{width:100vw;height:calc(100vw * 211 / 375);margin:0 0 10vh;}
   .img {
     display: none;
   }
   .imgM {
     display: block;
   }
+.if{width:100vw;height:calc(100vw * 211 / 375);margin:0 0 10vh;}
 }
 </style>
 
