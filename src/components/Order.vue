d54fb81ae5b97b6036618baae0f659f16db85afd
Zuray David <intheblackworld@gmail.com>
2021-01-08 14:05:33 +0800

    modify

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,66 +1,84 @@
 <template>
   <div class="order-bg">
     <!-- <div class="order-title" v-scroll-to="{ element: `#contact` }">{{order.title}}</div> -->
-    <h3 class="order-subtitle">{{order.subTitle}}</h3>
-    <div class="order">
+    <div class="section section-form">
+      <div class="title" data-aos="fade-up" data-aos-duration="1000">
+        <img :src="isMobile ? require('../pages/mobile-texture/section-form/title1.png') : require('../pages/texture/section-form/title1.png')" alt="2021市心質感高規宅 西門大院 LIFE WITH BOOKS & GARDEN" />
+      </div>
+      <div class="subtitle" data-aos="fade-up" data-aos-duration="1000">
+        <img src="../pages/texture/section-form/title2.png" alt="院中有園，圓滿一家子心願 即將公開 歡迎預約優先賞屋" v-if="!isMobile" />
+      </div>
+      <div class="bg">
+        <img src="../pages/texture/section-form/bg.png" alt="" v-if="!isMobile" />
+      </div>
+      <div class="form" data-aos="fade-up" data-aos-duration="1000">
+        <div class="left">
+          <div>
+            <label for="name">姓名</label>
+            <input type="text" name="" id="name" v-model="form.name" />
+          </div>
+          <div>
+            <label for="phone">手機</label>
+            <input type="text" name="" id="phone" v-model="form.phone" />
+          </div>
+          <div>
+            <label for="city">居住城市</label>
+            <select type="text" name="" id="city" v-model="form.city">
+              <option v-for="city in cityList" :key="city.value" :label="city.label" :value="city.value"></option>
+            </select>
+          </div>
+          <div>
+            <label for="county">居住地區</label>
+            <select type="text" name="" id="county" v-model="form.area">
+              <option v-for="area in areaList" :key="area.value" :label="area.label" :value="area.value"></option>
+            </select>
+          </div>
+        </div>
+        <div class="right">
+          <textarea name="" id="note" placeholder="請輸入您的留言 (選填)" cols="30" rows="10" v-model="form.msg"></textarea>
+        </div>
+        <div class="bottom">
+          <label class="policy-container" for="policy">
+            本人知悉並同意<span @click="showPolicyDialog">「個資告知事項聲明」</span>內容
+            <input type="checkbox" id="policy" v-model="checked" />
+            <span class="checkmark"></span>
+          </label>
+          <div id="send-form" class="hover" @click="submit" :loading="isSubmit">立即預約</div>
+        </div>
+      </div>
+    </div>
+    <!-- <h3 class="order-subtitle">{{order.subTitle}}</h3> -->
+    <!-- <div class="order">
       <div class="form">
         <div class="group">
           <div class="row">
             <label>姓名<span>*</span></label>
-            <el-input
-              v-model="form.name"
-              placeholder
-            ></el-input>
+            <el-input v-model="form.name" placeholder></el-input>
           </div>
           <div class="row">
             <label>手機<span>*</span></label>
-            <el-input
-              v-model="form.phone"
-              placeholder
-            ></el-input>
+            <el-input v-model="form.phone" placeholder></el-input>
           </div>
-          <!-- <div class="row">
+          <div class="row">
             <label>E-mail</label>
             <el-input v-model="form.email" placeholder></el-input>
-          </div> -->
+          </div>
           <div class="row">
             <label>居住城市</label>
-            <el-select
-              v-model="form.city"
-              placeholder
-            >
-              <el-option
-                v-for="city in cityList"
-                :key="city.value"
-                :label="city.label"
-                :value="city.value"
-              ></el-option>
+            <el-select v-model="form.city" placeholder>
+              <el-option v-for="city in cityList" :key="city.value" :label="city.label" :value="city.value"></el-option>
             </el-select>
           </div>
           <div class="row">
             <label>居住地區</label>
-            <el-select
-              v-model="form.area"
-              placeholder
-              no-data-text="請先選擇居住城市"
-            >
-              <el-option
-                v-for="area in areaList"
-                :key="area.value"
-                :label="area.label"
-                :value="area.value"
-              ></el-option>
+            <el-select v-model="form.area" placeholder no-data-text="請先選擇居住城市">
+              <el-option v-for="area in areaList" :key="area.value" :label="area.label" :value="area.value"></el-option>
             </el-select>
           </div>
         </div>
         <div class="group">
           <div class="row">
-            <el-input
-              type="textarea"
-              :rows="2"
-              placeholder="請輸入您的留言 (選填)"
-              v-model="form.msg"
-            ></el-input>
+            <el-input type="textarea" :rows="2" placeholder="請輸入您的留言 (選填)" v-model="form.msg"></el-input>
           </div>
         </div>
       </div>
@@ -74,22 +92,15 @@
         </el-checkbox>
       </div>
       <div style="margin: 0 auto">
-        <vue-recaptcha
-          :sitekey="info.recaptcha_site_key_v2"
-          @verify="isVerify = true"
-        ></vue-recaptcha>
+        <vue-recaptcha :sitekey="info.recaptcha_site_key_v2" @verify="isVerify = true"></vue-recaptcha>
       </div>
-      <el-button
-        class="form-submit"
-        type="primary"
-        :disabled="!checked || !isVerify"
-        @click="submit"
-        :loading="isSubmit"
-      >立即預約</el-button>
-      <!-- <Loading :loading="isSubmit" :isOpacity="true" /> -->
-    </div>
+      <el-button class="form-submit" type="primary" :disabled="!checked || !isVerify" @click="submit" :loading="isSubmit">立即預約</el-button>
+    </div> -->
     <ContactInfo />
-    <GoogleMap />
+    <div class="map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.218562810977!2d120.20789631542323!3d23.015745922357652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDAwJzU2LjciTiAxMjDCsDEyJzM2LjMiRQ!5e0!3m2!1szh-TW!2stw!4v1609495064404!5m2!1szh-TW!2stw" width="100%" height="600" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+    <!-- <GoogleMap /> -->
     <PolicyDialog :policyVisible="policyVisible" />
   </div>
 </template>
@@ -99,6 +110,7 @@ import ContactInfo from '@/components/ContactInfo.vue'
 import GoogleMap from '@/components/GoogleMap.vue'
 import PolicyDialog from '@/components/PolicyDialog.vue'
 import info from '@/info'
+import { isMobile } from '@/utils'
 import { cityList, renderAreaList } from '@/info/address'
 // import Loading from '@/components/Loading.vue'
 import VueRecaptcha from 'vue-recaptcha'
@@ -115,6 +127,7 @@ export default {
 
   data() {
     return {
+      isMobile,
       cityList,
       info,
       order: info.order,
@@ -160,7 +173,7 @@ export default {
 
     submit() {
       if (this.isSubmit) return
-      if (!this.isVerify) return
+      // if (!this.isVerify) return
       if (!this.checked) return
       this.isSubmit = true
       if (
@@ -209,7 +222,7 @@ export default {
       fetch('contact-form.php', {
         method: 'POST',
         body: formData,
-      }).then(response => {
+      }).then((response) => {
         this.isSubmit = false
         if (response.status === 200) {
           window.location.href = 'formThanks'
@@ -236,184 +249,183 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/style/variableColor.scss';
-.order-bg {
- // background-color: $order_bg_color;transparent
-  background-color: transparent;
-  background-image: $order_bg_image;
-  background-size: cover;
-  position: relative;
-  padding-top: 80px;
-  overflow: hidden;
+// @import '@/assets/style/variableColor.scss';
+// .order-bg {
+//   // background-color: $order_bg_color;transparent
+//   background-color: transparent;
+//   background-image: $order_bg_image;
+//   background-size: cover;
+//   position: relative;
+//   padding-top: 80px;
+//   overflow: hidden;
 
-  .order-title {
-    margin-top: 40px;
-    margin-bottom: 8px;
-    font-size: calc(100vw * 38 / 1920);
-    text-align: center;
-    color: $order_title_color;
-    width:calc(100vw * 500 / 1920);
-    background-color: #3d2824;
-    border-radius: 60px / 60px;
-    display: block;
-    margin: 40px auto 8px;
-    height: 60px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    position: relative;
-    z-index: 3;
-    &::before{
-     content: "";
-     position: absolute;
-     width: 1200px;
-     max-width: 95vw;
-     height: 1px;
-    background-color: #3d2824;
-    z-index: -1;
+//   .order-title {
+//     margin-top: 40px;
+//     margin-bottom: 8px;
+//     font-size: calc(100vw * 38 / 1920);
+//     text-align: center;
+//     color: $order_title_color;
+//     width: calc(100vw * 500 / 1920);
+//     background-color: #3d2824;
+//     border-radius: 60px / 60px;
+//     display: block;
+//     margin: 40px auto 8px;
+//     height: 60px;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     cursor: pointer;
+//     position: relative;
+//     z-index: 3;
+//     &::before {
+//       content: '';
+//       position: absolute;
+//       width: 1200px;
+//       max-width: 95vw;
+//       height: 1px;
+//       background-color: #3d2824;
+//       z-index: -1;
+//     }
+//   }
 
-    }
-  }
+//   .order-subtitle {
+//     font-size: 20px;
+//     text-align: center;
+//     color: $order_subtitle_color;
+//     margin-bottom: 40px;
+//     position: relative;
+//     z-index: 3;
+//   }
 
-  .order-subtitle {
-    font-size: 20px;
-    text-align: center;
-    color: $order_subtitle_color;
-    margin-bottom: 40px;
-    position: relative;
-    z-index: 3;
-  }
+//   .order {
+//     width: 920px;
+//     margin: 0 auto;
+//     display: flex;
+//     flex-direction: column;
+//     position: relative;
+//     z-index: 3;
+//   }
 
-  .order {
-    width: 920px;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    z-index: 3;
-  }
+//   .form {
+//     display: flex;
+//     align-items: center;
+//     > .group {
+//       flex: 1;
+//     }
+//   }
 
-  .form {
-    display: flex;
-    align-items: center;
-    > .group {
-      flex: 1;
-    }
-  }
+//   .group {
+//     height: 236px;
 
-  .group {
-    height: 236px;
+//     &:nth-child(1) {
+//       border-right: 1px solid rgba(0, 0, 0, 0.2);
+//       .row {
+//         justify-content: flex-start;
+//       }
+//     }
 
-    &:nth-child(1) {
-      border-right: 1px solid rgba(0, 0, 0, 0.2);
-      .row {
-        justify-content: flex-start;
-      }
-    }
+//     &:nth-child(2) {
+//       .row {
+//         justify-content: flex-end;
+//         height: 100%;
+//       }
+//     }
+//   }
 
-    &:nth-child(2) {
-      .row {
-        justify-content: flex-end;
-        height: 100%;
-      }
-    }
-  }
+//   .row {
+//     display: flex;
+//     align-items: center;
+//     margin-bottom: 15px;
 
-  .row {
-    display: flex;
-    align-items: center;
-    margin-bottom: 15px;
+//     &:nth-last-child(1) {
+//       margin-bottom: 0;
+//     }
 
-    &:nth-last-child(1) {
-      margin-bottom: 0;
-    }
+//     label {
+//       width: 92px;
+//       font-size: 16px;
+//       opacity: 0.8;
+//       color: $order_input_label_color;
 
-    label {
-      width: 92px;
-      font-size: 16px;
-      opacity: 0.8;
-      color: $order_input_label_color;
+//       span {
+//         color: #ff0000;
+//       }
+//     }
+//   }
 
-      span {
-        color: #ff0000;
-      }
-    }
-  }
+//   .control {
+//     margin-top: 60px;
+//     margin-bottom: 20px;
+//   }
+// }
 
-  .control {
-    margin-top: 60px;
-    margin-bottom: 20px;
-  }
-}
+// /* 平板尺寸 */
+// @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+//   .order-title {
+//     font-size: 32px;
+//   }
 
-/* 平板尺寸 */
-@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
-  .order-title {
-    font-size: 32px;
-  }
+//   .order-subtitle {
+//     font-size: 16px;
+//   }
 
-  .order-subtitle {
-    font-size: 16px;
-  }
+//   .order {
+//     width: 920px;
+//     margin: 0 auto;
+//   }
+// }
 
-  .order {
-    width: 920px;
-    margin: 0 auto;
-  }
-}
+// /* 螢幕尺寸標準 */
+// /* 手機尺寸 */
+// @media only screen and (max-width: 767px) {
+//   .order-bg {
+//     // background-image: $order_bg_image_m;
+//     background-color: transparent;
+//     background-size: auto 250vw;
+//     padding-top: 40px;
+//     .order-title {
+//       font-size: 18px;
+//       margin-top: 10px;
+//       margin-bottom: 20px;
+//       width: 90vw;
+//       height: 30px;
+//       position: relative;
+//       z-index: 2;
+//     }
 
-/* 螢幕尺寸標準 */
-/* 手機尺寸 */
-@media only screen and (max-width: 767px) {
-  .order-bg {
-    // background-image: $order_bg_image_m;
-    background-color: transparent;
-    background-size: auto 250vw;
-    padding-top: 40px;
-    .order-title {
-      font-size: 18px;
-      margin-top: 10px;
-      margin-bottom: 20px;
-      width: 90vw;
-      height: 30px;
-      position: relative;
-      z-index: 2;
-    }
+//     .order-subtitle {
+//       display: none;
+//     }
+//     .order {
+//       width: 95% !important;
+//       margin: 0 auto;
+//       padding: 0;
+//       position: relative;
+//       z-index: 2;
+//     }
 
-    .order-subtitle {
-      display: none;
-    }
-    .order {
-      width: 95% !important;
-      margin: 0 auto;
-      padding: 0;
-      position: relative;
-      z-index: 2;
-    }
+//     .form {
+//       flex-direction: column;
+//     }
 
-    .form {
-      flex-direction: column;
-    }
+//     .group {
+//       width: 100%;
+//       height: auto !important;
+//       margin-bottom: 0px !important;
+//       border: none !important;
+//     }
 
-    .group {
-      width: 100%;
-      height: auto !important;
-      margin-bottom: 0px !important;
-      border: none !important;
-    }
+//     .row {
+//       margin-bottom: 12px !important;
+//       label {
+//         width: 30% !important;
+//       }
+//     }
 
-    .row {
-      margin-bottom: 12px !important;
-      label {
-        width: 30% !important;
-      }
-    }
-
-    .control {
-      margin-top: 10px;
-      margin-bottom: 10px;
-    }
-  }
-}
+//     .control {
+//       margin-top: 10px;
+//       margin-bottom: 10px;
+//     }
+//   }
+// }
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,193 @@
   <div class="home no-padding-top">
     <div ref="gtmNoScript" />
     <Loading :loading="load" />
+    <div class="tilt section section-1">
+      <div class="title" v-if="!isMobile">
+        <img src="./texture/section-1/title1.png" alt="久等了!西門路。" />
+        <img src="./texture/section-1/title2.png" alt="台南首發！蔦屋書香跨界住宅 文元學區  2-4房 06-225-6688" />
+      </div>
+      <div class="title" v-if="isMobile">
+        <img src="./mobile-texture/section-1/title1.png" alt="久等了!西門路。" />
+        <img src="./mobile-texture/section-1/title2.png" alt="台南首發！蔦屋書香跨界住宅 文元學區  2-4房 06-225-6688" />
+      </div>
+      <div class="button1">
+        <a href=""><img src="./texture/section-1/r-button-1.png" v-scroll-to="{ element: `#contact`, offset: 0 }" alt="立即預約" /></a>
+        <a href=""><img src="./texture/section-1/r-button-2.png" alt="立即來電" v-scroll-to="{ element: `#contact`, offset: 0 }" /></a>
+      </div>
+      <div class="button2" v-if="!isMobile">
+        <a href=""><img src="./texture/section-1/c-button.png" alt="立即預約" v-scroll-to="{ element: `#contact`, offset: 0 }" /></a>
+      </div>
+      <div class="button2" v-if="isMobile">
+        <a href=""><img src="./mobile-texture/section-1/c-button.png" alt="立即預約" v-scroll-to="{ element: `#contact`, offset: 0 }" /></a>
+      </div>
+      <div class="subtitle" v-if="!isMobile">
+        <img src="./texture/section-1/title3.png" alt="您對家的夢想是甚麼？ 是奢華享受，是安心安全 孩子唸雙語好學校，有18座公園綠地盡情奔跑，家裡有超級大花園，還有全球最美書店選書
+一家子同居三代同堂幸福寬裕的好所在，您的心願，讓 〔西門大院〕 幫您完美實現" />
+      </div>
+      <div class="subtitle" v-if="isMobile">
+        <img src="./mobile-texture/section-1/title3.png" alt="您對家的夢想是甚麼？ 是奢華享受，是安心安全 孩子唸雙語好學校，有18座公園綠地盡情奔跑，家裡有超級大花園，還有全球最美書店選書
+一家子同居三代同堂幸福寬裕的好所在，您的心願，讓 〔西門大院〕 幫您完美實現" />
+      </div>
+      <div class="bg" data-aos="fade-down" data-aos-duration="2000" v-if="!isMobile">
+        <img src="./texture/section-1/bg.png" alt="" />
+      </div>
+      <div class="bg" data-aos="fade-down" data-aos-duration="2000" v-if="isMobile">
+        <img src="./mobile-texture/section-1/bg.png" alt="" />
+      </div>
+    </div>
+    <!--分隔-->
+    <div class="divider" data-aos="fade-down" data-aos-duration="2000">
+      <img src="./texture/divider.png" alt="" v-if="!isMobile" />
+      <img src="./mobile-texture/divider.png" alt="" v-else />
+    </div>
+    <!--滑動區塊-->
+    <div class="section section-swiper">
+      <div class="swiper-container" data-aos="fade-up" data-aos-duration="1000">
+        <div class="swipe-here">
+          <img src="./texture/swipe.png" alt="" v-if="!isMobile" />
+          <img src="./mobile-texture/swipe.png" alt="" v-else />
+        </div>
+        <div class="swiper-wrapper text-align-center" v-if="!isMobile">
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s2-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s2-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s2-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s2-1.png" alt="" />
+          </div>
+        </div>
+        <div class="swiper-wrapper text-align-center" v-else>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s2-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s2-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s2-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s2-1.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="title" data-aos="fade-left" data-aos-duration="1000">
+        <p class="swiper-intro-text-1">
+          台積電領頭擴大投資南科，古都台南正在3.0翻轉中！<br />
+          正西門路核心大道，貫通市心四通八達，15分通勤南科，科技新貴置產首選<br />
+          北區鄭子寮重劃區，西門三越商圈、COSTCO商圈、小北商圈繁華匯聚<br />
+          未來南山微風精品百貨、台南公園開發案潛力無限，現在入主正是起漲點
+        </p>
+        <img src="./texture/section-swiper/s2-title.png" alt="2021 許好願，買大院" v-if="!isMobile" />
+        <img src="./mobile-texture/section-swiper/s2-title.png" alt="2021 許好願，買大院" v-else />
+      </div>
+      <div class="swiper-container">
+        <div class="feature-title text-align-center">
+          <img src="./texture/section-swiper/s3-title.png" alt="" v-if="!isMobile" />
+          <img src="./mobile-texture/section-swiper/s3-title.png" alt="" v-else />
+        </div>
+        <div class="swipe-here">
+          <img src="./texture/swipe.png" alt="" v-if="!isMobile" />
+          <img src="./mobile-texture/swipe.png" alt="" v-else />
+        </div>
+        <div class="swiper-wrapper text-align-center" data-aos="fade-up" data-aos-duration="1000" v-if="!isMobile">
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s3-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s3-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s3-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s3-1.png" alt="" />
+          </div>
+        </div>
+        <div class="swiper-wrapper text-align-center" data-aos="fade-up" data-aos-duration="1000" v-else>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s3-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s3-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s3-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s3-1.png" alt="" />
+          </div>
+        </div>
+        <div class="swiper-pagination text-align-right"></div>
+      </div>
+      <div class="title flex-reverse" data-aos="fade-right" data-aos-duration="1000">
+        <p class="text-align-right swiper-intro-text-2">
+          以往到日本朝聖全球最美書店「蔦屋書店 」，現在在家就能享受<br />
+          台南首座與台灣蔦屋書店TSUTAYA BOOKSTORE攜手打造<br />
+          將現代和風生活提案帶入〔西門大院〕書香人文空間<br />
+          包括四季大花園、台灣蔦屋書房、健身房、兒童遊戲閱覽室<br />
+          才藝教室、閱覽室、空中花園等豐富的樂齡共享休閒設施
+        </p>
+        <img src="./texture/section-swiper/s3-title.png" alt="台日攜手西門大院×臺灣蔦屋" v-if="!isMobile" />
+        <img src="./mobile-texture/section-swiper/s3-title.png" alt="台日攜手西門大院×臺灣蔦屋" v-else />
+      </div>
+      <div class="swiper-container">
+        <div class="feature-title text-align-center" data-aos="fade-up" data-aos-duration="1000">
+          <img src="./texture/section-swiper/s4-title.png" alt="" v-if="!isMobile" />
+          <img src="./mobile-texture/section-swiper/s4-title.png" alt="" v-else />
+        </div>
+        <div class="swipe-here">
+          <img src="./texture/swipe.png" alt="" v-if="!isMobile" />
+          <img src="./mobile-texture/swipe.png" alt="" v-else />
+        </div>
+        <div class="swiper-wrapper text-align-center" data-aos="fade-right" data-aos-duration="1000" v-if="!isMobile">
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s4-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s4-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s4-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./texture/section-swiper/s4-1.png" alt="" />
+          </div>
+        </div>
+        <div class="swiper-wrapper text-align-center" data-aos="fade-right" data-aos-duration="1000" v-else>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s4-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s4-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s4-1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./mobile-texture/section-swiper/s4-1.png" alt="" />
+          </div>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+      <div class="title" data-aos="fade-left" data-aos-duration="1000">
+        <p class="swiper-intro-text-3">
+          座落豪宅林立鄭子寮高級住宅生活聚落，18座公園綠意環繞<br />
+          市心難得低密度高綠覆開發，文元國小高升學率明星學區<br />
+          禮聘台北名宅大師團隊打造，正西門路凌空24層雙塔地標<br />
+          千坪開闊大基地，三面臨路好風光，高坪效精工制震宅<br />
+          等待是值得的，鄭子寮最美的日系花園建築，2021即將美好盛開
+        </p>
+        <img src="./texture/section-swiper/s4-title.png" alt="鄭子寮富人特區 凌空24層制震豪邸" v-if="!isMobile" />
+        <img src="./mobile-texture/section-swiper/s4-title.png" alt="鄭子寮富人特區 凌空24層制震豪邸" v-else />
+      </div>
+    </div>
+
     <!-- <SideNavigation v-if="isSide" /> -->
     <!-- <Navigation /> -->
     <!-- <div id="section1">
@@ -22,17 +209,19 @@
     <div id="section6">
       <Section6 />
     </div> -->
-    <div id="section7">
+    <!-- <div id="section7">
       <Section7 />
-    </div>
+    </div> -->
     <ContactSection />
     <!-- <SimpleOrder /> -->
-    <MobileNav />
+    <!-- <MobileNav /> -->
   </div>
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=Playball&display=swap');
+@import url('./css/style.css');
+@import url('./css/swiper-bundle.min.css');
 </style>
 
 <script>
@@ -41,6 +230,7 @@
 import ContactSection from '@/layouts/ContactSection.vue'
 import MobileNav from '@/layouts/MobileNav.vue'
 import Loading from '@/components/Loading.vue'
+import { isMobile } from '@/utils'
 import gtm from '@/mixins/gtm.js'
 
 // import Section1 from '@/projects/hg/Section1.vue'
@@ -71,6 +261,7 @@ export default {
 
   data() {
     return {
+      isMobile,
       isSide: true,
       load: true,
     }
@@ -81,6 +272,10 @@ export default {
     })
 
     // window.location = "https://ywh.nhc888.com.tw/"
+  },
+
+  mounted() {
+    console.log($)
   },
 
   methods: {
