9c9add1c5d1bf1288ceb58b4bb8a615b22c779cc
Zuray David <intheblackworld@gmail.com>
2021-01-20 20:52:31 +0800

    modify

--- src/info/index.js
+++ src/info/index.js
@@ -21,7 +21,7 @@ export default {
     ['樓層規劃', '地下6層',],
   ],
 
-  gtmCode: ['WG7P8LC'], // 可放置多個
+  gtmCode: ['KVNRLFH'], // 可放置多個
   recaptcha_site_key_v2: '6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC',
   recaptcha_site_key: '6LfGUjEaAAAAAFR9F5m6Lg1RfS-D07l64KWmoLcK', // recaptcha v3
   recaptcha_user_token: '6LfGUjEaAAAAAFR9F5m6Lg1RfS-D07l64KWmoLcK',
