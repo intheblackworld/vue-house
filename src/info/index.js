19c3d67fdd423a9430dde595cfdc631ed660a015
xcatx <xcatx796@gmail.com>
2021-01-24 18:37:50 +0800

    lj

--- src/info/index.js
+++ src/info/index.js
@@ -5,8 +5,8 @@ export default {
     'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3616.0773765100757!2d121.5492322!3d24.9974866!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442aa0fe568f589%3A0x93d301522de5cebe!2zMTE25Y-w5YyX5biC5paH5bGx5Y2A5LuZ5bKp6LevMTblt7czMuiZnw!5e0!3m2!1szh-TW!2stw!4v1610444094269!5m2!1szh-TW!2stw',
   googleLink: 'https://goo.gl/maps/NnSKHBWtj54jKc4g7',
   phone: '02-89322888',
-  fbLink: 'https://www.facebook.com/',
-  fbMessage: 'https://m.me/',
+  fbLink: 'https://www.facebook.com/%E9%80%A3%E9%9B%B2%E6%99%AF%E7%B2%8B-105504811458953',
+  fbMessage: 'https://m.me/105504811458953',
   caseName: '連雲景粋',
   indigatorLength: 10,
 
