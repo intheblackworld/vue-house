35caa3e28918ccddd7e76d6c56be8bb7f3400b4b
xcatx <xcatx796@gmail.com>
2021-01-27 21:57:08 +0800

    sm

--- src/info/index.js
+++ src/info/index.js
@@ -1,14 +1,14 @@
 // import { isMobile } from '@/utils'
 
 export default {
-  address: '接待會館：新北市五股區新城五路與新城八路交叉',
+  address: '接待會館：臺南市北區西門路四段357號',
   // isMobile ? '接待中心：<br />106台北市大安區基隆路三段20-1號' : '接待中心：106台北市大安區基隆路三段20-1號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.293723920597!2d121.44400571738508!3d25.09191669899662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDA1JzMwLjkiTiAxMjHCsDI2JzQ2LjMiRQ!5e0!3m2!1szh-TW!2stw!4v1590128993509!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/tg65p8Gxc8kamXmW9',
-  phone: '02-2608-6985',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.221454243889!2d120.20803281535407!3d23.01563972236179!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e76f951fa95b9%3A0x7db91a148c47a54e!2zNzA05Y-w5Y2X5biC5YyX5Y2A6KW_6ZaA6Lev5Zub5q61MzU36Jmf!5e0!3m2!1szh-TW!2stw!4v1611755799328!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/kdsdDfEZb3GyvJWc7',
+  phone: '06 225 6688',
   fbLink:
-    'https://www.facebook.com/%E8%A5%BF%E9%96%80%E5%A4%A7%E9%99%A2-101474465251897',
+    'https://www.facebook.com/%E8%A5%BF%E9%96%80%E5%A4%A7%E9%99%A2-101474465251897/',
   fbMessage: 'https://m.me/101474465251897',
   caseName: '西門大院',
   houseInfos: [
