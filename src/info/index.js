6d0c01e083fbbd8e233f96e98615cb65accffb2d
Zuray David <intheblackworld@gmail.com>
2021-02-03 13:19:53 +0800

    modify

--- src/info/index.js
+++ src/info/index.js
@@ -21,7 +21,7 @@ export default {
     ['經紀人', '徐福龍 (91) 北市經證字第00848號'],
   ],
 
-  gtmCode: ['MGNM9FV'], // 可放置多個
+  gtmCode: ['W95H7JW'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
