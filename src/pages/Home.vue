eb7906316f10725f9a85682035b6aa79cc345114
Zuray David <intheblackworld@gmail.com>
2021-03-10 22:34:30 +0800

    modify

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -190,7 +190,7 @@ export default {
           navIndex = n
         }
       }
-      if (this.viewIndex === navIndex + 1) {
+      if (this.viewIndex !== navIndex + 1) {
         this.viewIndex = navIndex + 1
       }
       // this.viewIndex = navIndex + 1
