9771f746e1b94549deb3d5fa94d0c938b61e20fe
xcatx <xcatx796@gmail.com>
2021-03-15 17:49:50 +0800

    忘了換資訊

--- src/info/index.js
+++ src/info/index.js
@@ -2,12 +2,12 @@ import { isMobile } from '../utils/index'
 export default {
   address: '台北市松江路252巷',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3616.0773765100757!2d121.5492322!3d24.9974866!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442aa0fe568f589%3A0x93d301522de5cebe!2zMTE25Y-w5YyX5biC5paH5bGx5Y2A5LuZ5bKp6LevMTblt7czMuiZnw!5e0!3m2!1szh-TW!2stw!4v1610444094269!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/NnSKHBWtj54jKc4g7',
-  phone: '02-29668888',
-  fbLink: 'https://www.facebook.com/%E9%80%A3%E9%9B%B2%E6%99%AF%E7%B2%8B-105504811458953',
-  fbMessage: 'https://m.me/105504811458953',
-  caseName: '連雲景粋',
+    'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3614.258618139814!2d121.5325556!3d25.0592222!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDAzJzMzLjIiTiAxMjHCsDMxJzU3LjIiRQ!5e0!3m2!1szh-TW!2stw!4v1615801563403!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/jRV143mcJBkciBKx7',
+  phone: '02-86676688',
+  fbLink: 'https://www.facebook.com/佳元松江-103677951795416',
+  fbMessage: 'https://m.me/103677951795416',
+  caseName: '佳元松江',
   indigatorLength: 10,
 
   houseInfos: [
@@ -21,7 +21,7 @@ export default {
     ['經紀人', '戴子順 (96)竹縣地經字第00150號'],
   ],
 
-  gtmCode: ['MGNM9FV'], // 可放置多個
+  gtmCode: [''], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
