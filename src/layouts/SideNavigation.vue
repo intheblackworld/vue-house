a5f9b0a8b4ab095ee7001fade7776a328f4bd4d5
Zuray David <intheblackworld@gmail.com>
2021-03-29 15:51:51 +0800

    modify

--- src/layouts/SideNavigation.vue
+++ src/layouts/SideNavigation.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="sideNav">
-    <div :class="`nav relative ${isOpen ? 'open' : ''}`">
+    <div :class="`nav ${isOpen ? 'open' : ''}`">
       <div class="menu" @click="toggleSidebar">
         <span data-v-8ecc579a="" class="menu_icon"></span>
-        <span data-v-8ecc579a="" class="menu_text">MENU</span>
-
+        <span data-v-8ecc579a="" class="menu_text"></span>
+        <img src="../projects/hy1/s1/menulogo.png" alt="" class="logo">
       </div>
       <div :class="`mask ${isOpen ? 'open' : ''}`" @click="toggleSidebar" />
       <ul :class="`navlist ${isOpen ? 'open': ''}`">
-        <li
-          :key="item.name"
-          v-scroll-to="{ element: `#${item.section}`, offset: offset }"
-          v-for="item in list"
-          class="flex-ac"
-          @click="toggleSidebar"
-        >
+        <li :key="item.name" v-scroll-to="{ element: `#${item.section}`, offset: offset }" v-for="item in list" class="flex-ac" @click="toggleSidebar">
           <span class="link">
             <img v-if="item.imgSrc" :src="item.imgSrc" alt />
             <span>
-              <p class="title">{{item.name}}</p>
+              <h3 class="title">{{item.name}}</h3>
               <span class="subTitle">{{item.subTitle}}</span>
             </span>
           </span>
@@ -61,6 +55,7 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/style/variableColor.scss';
 @import '../assets/style/variableDefault.scss';
+@import '../assets/style/function.scss';
 
 .navigation {
   height: $nav_phone_height;
@@ -79,7 +74,8 @@ export default {
 
 .nav {
   position: static;
-  height: $nav_phone_height;
+  top: 0;
+  left: 0;
 }
 
 .menu {
@@ -92,6 +88,32 @@ export default {
   width: 80px;
   cursor: pointer;
 
+  &::after {
+    content: '';
+    position: fixed;
+    width: 100vw;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    z-index: 111;
+    height: size(118);
+    background: transparent;
+    z-index: 100;
+    border-style: solid;
+    border-width: size(118) 100vw 0 0;
+    border-color: #c40000 transparent transparent transparent;
+    transition: all 0.3s;
+  }
+
+  .logo {
+    position: absolute;
+    width: size(102);
+    top: size(24);
+    left: size(111);
+    z-index: 110;
+    transition: all 0.3s;
+  }
+
   img {
     width: 100%;
   }
@@ -102,63 +124,104 @@ export default {
     margin-right: 0px;
   }
 }
- .menu_icon,.menu_icon::after,.menu_icon::before,{
-        position: absolute;
-        display: block;
-        width: 100%;
-        height: 1px;
-        left: 0;
-        background-color: #000;
-        content: "";
-        transition: all .3s;
-        top: 0
- }
- .menu_icon{
-        -webkit-transition: all .3s;
-        transition: all .3s;
-        text-indent: -9999px;
-        line-height: 0;
-        top: 40%;
-        left: 25%;width: 50%;
-        margin-top:-2px; 
- }
+.menu_icon,
+.menu_icon::after,
+.menu_icon::before {
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: size(3);
+  left: 0;
+  background-color: #fff;
+  content: '';
+  transition: all 0.3s;
+  top: 0;
+  z-index: 112;
+}
+.menu_icon {
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+  text-indent: -9999px;
+  line-height: 0;
+  top: 40%;
+  left: 25%;
+  width: 50%;
+  margin-top: -2px;
+}
 
- .menu_icon::before {
-        -webkit-transform: translateY(1000%);
-        transform: translateY(1000%)
- }
+.menu_icon::before {
+  -webkit-transform: translateY(400%);
+  transform: translateY(400%);
+}
 
- .menu_icon::after {
-        -webkit-transform: translateY(-1000%);
-        transform: translateY(-1000%)
- }
- .menu_text{display: block;font-size: 1em;
-   font-family: "Barlow Condensed";font-weight: bold;margin: 50px 0 20px 0;transition: all 0.5s;overflow: hidden
+.menu_icon::after {
+  -webkit-transform: translateY(-400%);
+  transform: translateY(-400%);
+}
+.menu_text {
+  display: block;
+  font-size: 1em;
+  font-family: 'Barlow Condensed';
+  font-weight: bold;
+  margin: size(90) 0 size(20) 0;
+  transition: all 0.5s;
+  overflow: hidden;
+}
+
+// .nav {
+//   &.open {
+//     &::after {
+//       content: '';
+//       position: fixed;
+//       width: 100vw;
+//       top: 0;
+//       left: 0;
+//       width: 100vw;
+//       z-index: 111;
+//       height: size(118);
+//       background: transparent;
+//       z-index: 100;
+//       border-style: solid;
+//       border-width: size(968) 100vw 0 0;
+//       border-color: #c40000 transparent transparent transparent;
+//     }
+//   }
+// }
+.open {
+  .menu_icon {
+    width: 0;
   }
-  .open{
-     .menu_icon {width:0;
-    }
 
-    .menu_icon:before {opacity: 0
-    }
+  .logo {
+    width: size(204);
+  }
 
-     .menu_icon:after {opacity: 0
-    }
-    .menu_text{transform: translate(0) rotate(90deg);font-size: 1.5em;}
-    }
+  .menu_icon:before {
+    opacity: 0;
+  }
+
+  .menu_icon:after {
+    opacity: 0;
+  }
+  .menu_text {
+    transform: translate(0) rotate(90deg);
+    font-size: 1.5em;
+  }
+}
 .navlist {
   position: fixed;
   z-index: 111;
-  width:0;overflow: hidden;
-  left:25px;
+  width: 0;
+  overflow: hidden;
+  left: 25px;
   top: $nav_phone_height;
   height: calc(100vh - #{$nav_phone_height});
   text-align: center;
   transition: all 1s ease-in;
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
   transform: translateX(0%);
-    align-content: center;
+  align-content: center;
 
   li {
     height: 60px;
@@ -177,7 +240,7 @@ export default {
     background-size: cover;
     position: relative;
     cursor: pointer;
-    padding: 0 50px 0 0;
+    padding: 0 20px 0 0;
 
     img,
     span {
@@ -185,58 +248,76 @@ export default {
     }
     span {
       line-height: 16px;
-        white-space: nowrap;
+      white-space: nowrap;
     }
-    &::before{
+
+
+    &::before {
       position: absolute;
       display: block;
-      right:205px;
-      content: "";
+      right: 185px;
+      content: '';
       top: calc(50% - 0.5em);
-      width: 25px;height: 1px;
-      opacity:1;
-      transition: all .3s;
-      border: 1px solid #000;border-width: 1px 1px 0 0;
-    }
-    &:hover:before{
-      background:none;width: 15px;height:15px;border-color: #c00;transform:rotate(45deg);
-      animation: menu_l 0.3s infinite alternate ease-out;
-    } 
-
-      .subTitle {
-        font-family: 'Barlow Condensed', sans-serif;
-        margin-top: 5px;
-        color: $nav_link_subtitle_color;
-      }
-      .title {
-        color: $nav_link_color;
-      }
-}
-@keyframes menu_l{
-    to{
-        right:200px;
-    }
-}
-
-  &::before{
-      position: absolute;
-      display: block;
-      left: 0;
-      background-color: #000;
-      content: "";
-      top: 0;
-      width: 1px;height: 0px;
-      opacity: 0;
-      transition: all 1s;
-      }
-  &.open::before{
-      height:calc(50vh + 190px);
+      width: 45px;
+      height: 1px;
       opacity: 1;
-      }
+      transition: all 0.3s;
+      border: 1px solid #fff;
+      border-width: 1px 1px 0 0;
+    }
+    &:hover:before {
+      background: none;
+      width: 15px;
+      height: 15px;
+      border-color: #c00;
+      transform: rotate(45deg);
+      animation: menu_l 0.3s infinite alternate ease-out;
+    }
+
+    .subTitle {
+      font-family: 'Barlow Condensed', sans-serif;
+      margin-top: 5px;
+      color: $nav_link_subtitle_color;
+    }
+    .title {
+      font-size: size(30);
+      font-weight: bold;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: 1.33;
+      letter-spacing: size(0.3);
+      text-align: left;
+      color: $nav_link_color;
+      margin-top: size(-10);
+      color: #000;
+    }
+  }
+  @keyframes menu_l {
+    to {
+      right: 200px;
+    }
+  }
+
+  &::before {
+    position: absolute;
+    display: block;
+    left: 0;
+    background-color: #fff;
+    content: '';
+    top: 0;
+    width: 1px;
+    height: 0px;
+    opacity: 0;
+    transition: all 1s;
+  }
+  &.open::before {
+    height: calc(50vh + 223px);
+    opacity: 1;
+  }
   &.open {
-    width:230px;
+    width: 230px;
     transform: translateX(0%);
-   opacity: 1;
+    opacity: 1;
 
     .link {
       .divided {
@@ -244,7 +325,8 @@ export default {
       }
 
       &:hover {
-        .title,.subTitle {
+        .title,
+        .subTitle {
           color: $nav_link_hover_color;
         }
       }
@@ -262,10 +344,26 @@ export default {
   height: calc(100vh - #{$nav_phone_height});
   opacity: 0;
   transition: all 0.3s ease-in;
-  background: -moz-linear-gradient(left,  rgba(255,255,255,0.8) 0%, rgba(255,255,255,0.8) 50%, rgba(255,255,255,0) 100%);
-background: -webkit-linear-gradient(left,  rgba(255,255,255,0.8) 0%,rgba(255,255,255,0.8) 50%,rgba(255,255,255,0) 100%);
-background: linear-gradient(to right,  rgba(255,255,255,0.8) 0%,rgba(255,255,255,0.8) 50%,rgba(255,255,255,0) 100%);
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ccffffff', endColorstr='#00ffffff',GradientType=1 );
+  background: rgba(0, 0, 0, .6);
+  // background: -moz-linear-gradient(
+  //   left,
+  //   rgba(255, 255, 255, 0.8) 0%,
+  //   rgba(255, 255, 255, 0.8) 50%,
+  //   rgba(255, 255, 255, 0) 100%
+  // );
+  // background: -webkit-linear-gradient(
+  //   left,
+  //   rgba(255, 255, 255, 0.8) 0%,
+  //   rgba(255, 255, 255, 0.8) 50%,
+  //   rgba(255, 255, 255, 0) 100%
+  // );
+  // background: linear-gradient(
+  //   to right,
+  //   rgba(255, 255, 255, 0.8) 0%,
+  //   rgba(255, 255, 255, 0.8) 50%,
+  //   rgba(255, 255, 255, 0) 100%
+  // );
+  // filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ccffffff', endColorstr='#00ffffff',GradientType=1 );
 
   &.open {
     display: block;
@@ -277,7 +375,7 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ccffffff', e
 
 /* 平板尺寸 */
 @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
- /* .navigation {
+  /* .navigation {
     height: $nav_tablet_height;
     justify-content: center;
   }
@@ -311,7 +409,7 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ccffffff', e
 /* 螢幕尺寸標準 */
 /* 手機尺寸 */
 @media only screen and (max-width: 767px) {
- /* .navigation {
+  /* .navigation {
     height: $nav_phone_height;
     z-index: 110;
   }
