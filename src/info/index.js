ea09e71565a862e8e1d622b0bfbec6125a46e9c2
molly <molly8358158@gmail.com>
2021-04-06 09:26:30 +0800

    修改FB網址

--- src/info/index.js
+++ src/info/index.js
@@ -5,7 +5,7 @@ export default {
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.2907122557103!2d121.44305321542826!3d25.092018683945213!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a601fb82c025%3A0xa0eb02e41c8374ec!2zMjQ45paw5YyX5biC5LqU6IKh5Y2A5oiQ5rOw6Lev5LiJ5q61MTcx6Jmf!5e0!3m2!1szh-TW!2stw!4v1612155242808!5m2!1szh-TW!2stw',
   googleLink: 'https://goo.gl/maps/BEp2gd6h3otsGDwd6',
   phone: '02-2292-2255',
-  fbLink: 'https://m.facebook.com/%E5%A4%A9%E7%A9%BA%E4%B9%8B%E9%82%91-102772851839341/',
+  fbLink: 'https://www.facebook.com/sky0222922255/',
   fbMessage: 'https://m.me/102772851839341',
   caseName: '天空之邑',
   indigatorLength: 10,
