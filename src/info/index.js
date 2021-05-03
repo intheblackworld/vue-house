0f15c35ebb6e593f764598c3647127e9cd288f69
xcatx <xcatx796@gmail.com>
2021-05-03 15:52:31 +0800

    換地址

--- src/info/index.js
+++ src/info/index.js
@@ -1,9 +1,9 @@
 import { isMobile } from '../utils/index'
 export default {
-  address: '接待中心：台灣新北市蘆洲區光榮路120巷1號',
+  address: '接待中心：台灣新北市蘆洲區三民路557號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.3774958345693!2d121.46310273834531!3d25.089079749129265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a8b459f2ebcd%3A0xf900cb978e851b8e!2zMjQ35paw5YyX5biC6JiG5rSy5Y2A5YWJ5qau6LevMTIw5be3MeiZnw!5e0!3m2!1szh-TW!2stw!4v1609745909583!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/iZvYMG3MbN9TZzrF7',
+    'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3613.2842336027143!2d121.4603302459163!3d25.09223807128095!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a8b350bcd1cf%3A0x3f2e6514f1b1c0ad!2zMjQ35paw5YyX5biC6JiG5rSy5Y2A5LiJ5rCR6LevNTU36Jmf!5e0!3m2!1szh-TW!2stw!4v1620028059816!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/EhW47pbfTUJXeVbp7',
   phone: '02-8287-6888',
   fbLink: 'https://www.facebook.com/%E4%B9%9D%E6%8F%9A%E7%9B%A7%E7%99%BB%E5%A0%A1-102628871717373/',
   fbMessage: 'https://m.me/102628871717373',
