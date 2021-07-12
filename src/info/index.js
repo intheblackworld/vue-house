446cd124b5b4311bfb0afac7c751ea1d35695287
xcatx <xcatx796@gmail.com>
2021-07-12 17:31:08 +0800

    gtm

--- src/info/index.js
+++ src/info/index.js
@@ -35,7 +35,7 @@ export default {
       ['座落位置', '台北市信義區吳興街524巷',],*/
   ],
 
-  gtmCode: ['NC3LPM7'], // 可放置多個
+  gtmCode: ['NWPPWXL'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
