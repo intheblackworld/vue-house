5e96935819c11b02f1283fca66be6d38ec921c84
Zuray David <intheblackworld@gmail.com>
2021-07-17 06:11:22 +0800

    modify

--- src/lib/aos/src/js/aos-config.js
+++ src/lib/aos/src/js/aos-config.js
@@ -1,5 +1,5 @@
 export default {
-  startEvent: 'load', // 監聽網頁讀取事件 DOMContentLoaded(較快), load(較慢)
+  startEvent: 'DOMContentLoaded', // 監聽網頁讀取事件 DOMContentLoaded(較快), load(較慢)
   once: false, // 出現一次
   mirror: false, // 出現一次
   delay: 0,
