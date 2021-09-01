ae7a37862ab1f480958604cf319e72f61acc0e94
xcatx <xcatx796@gmail.com>
2021-09-01 10:21:31 +0800

    gtn

--- src/info/index.js
+++ src/info/index.js
@@ -17,7 +17,7 @@ export default {
     ['景觀設計', '六國景觀'],
   ],
 
-  gtmCode: [' '], // 可放置多個
+  gtmCode: ['MBMBGP4'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
