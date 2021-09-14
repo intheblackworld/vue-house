cfafa78e215e8204bb0749bb826835d8559f2eb7
Zuray David <intheblackworld@gmail.com>
2021-09-14 13:48:08 +0800

    modify

--- src/info/index.js
+++ src/info/index.js
@@ -1,33 +1,29 @@
 // import { isMobile } from '@/utils'
 
 export default {
-  address: '接待會館：台北市中正區忠孝東路二段22號',
+  address: '高雄市前金區六合二路98號',
   // isMobile ? '接待中心：<br />106台北市大安區基隆路三段20-1號' : '接待中心：106台北市大安區基隆路三段20-1號',
   googleSrc:
     'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7229.442801339142!2d121.5252484!3d25.0435271!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a97af1b8b009%3A0xba2c0a9a93d4c2d8!2zMTAw5Y-w5YyX5biC5Lit5q2j5Y2A5b-g5a2d5p2x6Lev5LqM5q61MjLomZ8!5e0!3m2!1szh-TW!2stw!4v1615173040430!5m2!1szh-TW!2st',
   googleLink: 'https://goo.gl/maps/2y84Bx5YEPRqFfBi8',
-  phone: '(02)2391-5577',
+  phone: '07-2852111',
   fbLink:
     'https://www.facebook.com/中正H25-102637598419351/',
   fbMessage: 'https://m.me/102637598419351/',
-  caseName: '中正H25',
+  caseName: '達麗東京',
   houseInfos: [
-    ['投資興建', '力天開發股份有限公司'],
-    ['建築設計', '呂大吉建築師事務所、許華山建築師事務所'],
-    ['接待中心', '台北市中正區忠孝東路二段22號'],
-    ['建照號碼', '109建字第0215號'],
-    ['企劃銷售', '群旺廣告'],
-    ['公設比例', '37%'],
-    ['基地面積', '57坪'],
-    ['格局規劃', '1房(13-17坪)'],
-    ['樓層規劃', '地上7樓，地下2層'],
+    ['投資興建', '達麗建設事業股份有限公司'],
+    ['建築規劃', '407戶住家，９戶店面'],
+    ['使照號碼', '(110)高市工建築使字第01284號'],
+    ['工程營造', '寶信營造股份有限公司'],
+    ['廣告銷售', '上揚國際建築團隊'],
   ],
   gtmCode: ['52F5VGR'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
   order: {
-    title: '預約賞屋' ,
+    title: '預約賞屋',
     subTitle: '若想了解建案資訊，歡迎填寫表單或來電洽詢，將由專人為您服務，謝謝！'
   },
 }
