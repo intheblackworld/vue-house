e317fd0694d611e06e8755a816050e7e866c4a26
xcatx <xcatx796@gmail.com>
2021-10-07 16:28:09 +0800

    ryc

--- src/projects/ryc/Section1.vue
+++ src/projects/ryc/Section1.vue
@@ -6,6 +6,37 @@
     <img src="./s1/k.png" class="img2" alt="睿暘沁_k"
           data-aos="fade"
           data-aos-delay="100">
+<svg class="img2 gifimg"
+          data-aos="fade"
+          data-aos-delay="100" version="1.1" id="圖層_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 718 546" style="enable-background:new 0 0 718 546;" xml:space="preserve">
+
+<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="62.6866" y1="85.1198" x2="603.8442" y2="397.5573">
+	<stop  offset="0" stop-color="#FFF0"/>
+      <stop offset="50%" stop-color="#FFF0" stop-opacity=".5">
+        <animate attributeName="offset" values="0;.0;.6;1;1;1;1;1;1" dur="3s" repeatCount="indefinite" />
+      </stop>
+      <stop offset="50%" stop-color="#FFF" stop-opacity=".5">
+        <animate attributeName="offset" values="0;.1;.7;1;1;1;1;1;1" dur="3s" repeatCount="indefinite" />
+      </stop>
+      <stop offset="50%" stop-color="#FFF0" stop-opacity=".5">
+        <animate attributeName="offset" values="0;.3;.9;1;1;1;1;1;1" dur="3s" repeatCount="indefinite" />
+      </stop>
+      <stop offset="100%" stop-color="#FFF0"> </stop>
+</linearGradient>
+<path fill="url(#SVGID_1_)" d="M80.7,126.4c-26.3,16.9-51.4,36.5-75,58.7c-6.2,5.8-6.3,15.6-0.3,21.6l20.4,20.4L80.7,126.4z M699.5,220.6
+	l13.9-13.9c6-6,5.9-15.8-0.3-21.6c-24.1-22.7-49.9-42.7-76.9-59.9L699.5,220.6z M366.3,179.2l97.9,100.1h-98L366.3,179.2z
+	 M560.3,136.3l-85.1,141.9L593.3,252L560.3,136.3z M466.4,284.1l-100.3,0L365.7,546L466.4,284.1z M470,277.6l86.2-143.7l-0.3-0.2
+	L369,174.2L470,277.6z M257.6,277.7l101.1-103.5L157,134.9L257.6,277.7z M148.3,137.3L31.6,232.8l2.5,2.5l91.9,18.1L148.3,137.3z
+	 M361.5,179.2l-97.9,100.1h97.7L361.5,179.2z M597.9,250.9l91.6-20.4l3.9-3.9l-128.4-90.9L597.9,250.9z M330.3,45.6
+	c-14,0.8-28.1,2.1-42,4.1l-35,6.1c-23.6,4.9-47,11.6-69.9,19.8l-24,46.2L330.3,45.6z M562.5,127.4l59.1-8.3l-54.9-31
+	c-10.3-4.5-20.7-8.7-31.1-12.5L562.5,127.4z M363.9,170.4l185.3-40.1L379.8,45.1c-11.5-0.5-23.1-0.5-34.7-0.2l-189.8,84.6l-0.1,0.2
+	L363.9,170.4z M568.2,132.1l124.5,88.1l-64-96.6L568.2,132.1z M145.1,133.7l-56.8-10L32.9,225.6L145.1,133.7z M150.2,129l1.3-2.4
+	l-0.5-1.1l1.4-0.6L176.8,78c-11.3,4.3-22.5,9-33.5,14.1l-48.3,27.2L150.2,129z M126.9,258.5l-86.8-17.1l132,132l168.3,160.9
+	L126.9,258.5z M472.5,283.7L372.4,544.1L590,257.6L472.5,283.7z M597.2,256L385.9,534.1l3.8-3.7l293.6-293.6L597.2,256z
+	 M361.3,284.1l-100.1,0L361,546L361.3,284.1z M557.6,128.4L557.6,128.4l-28.7-55.2C485,58,439.3,48.9,393.4,45.9L557.6,128.4z
+	 M153.1,137.6l-22.5,116.7l121.5,23.9L153.1,137.6z M255.3,283.8L134,259.9l220.6,284.9L255.3,283.8z"/>
+</svg>
     <img src="./s1/l.png" class="img1" alt="睿暘沁_l"
           data-aos="fade"
           data-aos-delay="100">
