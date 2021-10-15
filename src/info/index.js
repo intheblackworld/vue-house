f5051602bc141b409661c88765cf8ece268e9fdc
molly <molly8358158@gmail.com>
2021-10-15 14:26:28 +0800

    修改地圖

--- src/info/index.js
+++ src/info/index.js
@@ -1,8 +1,8 @@
 export default {
   address: '台南安平區健康路三段VS.國平路',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14692.350182374417!2d120.1703851!3d22.983808!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5824013f2fb46bbf!2z6IGv5LiK5rW35qOg!5e0!3m2!1szh-TW!2stw!4v1593007312077!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/YkMPpXwgPeh3kNke6',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.083837190087!2d120.16795021496706!3d22.983944384972727!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjLCsDU5JzAyLjIiTiAxMjDCsDEwJzEyLjUiRQ!5e0!3m2!1szh-TW!2stw!4v1634279092262!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/ZGYjcUMxGgF2gGnv8',
   phone: '06-299-2788',
   fbLink: 'https://www.facebook.com/VVVVV.HAI.TANG/',
   fbMessage: 'http://m.me/VVVVV.HAI.TANG/',
