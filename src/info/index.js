a72e93185e7dc2c03e33b0e9a5867ca43bf2d0f9
xcatx <xcatx796@gmail.com>
2021-11-22 11:18:33 +0800

    cc

--- src/info/index.js
+++ src/info/index.js
@@ -2,8 +2,8 @@ import { isMobile } from '../utils/index'
 export default {
   address: '接待中心：新北市新莊區昌德街33號旁',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d903.5962551936593!2d121.45608382922413!3d25.05493621390746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDAzJzE3LjgiTiAxMjHCsDI3JzIzLjkiRQ!5e0!3m2!1szh-TW!2stw!4v1634204528398!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/9iPuMyNdg2G6niwU7',
+    'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3614.3847262875265!2d121.4549993!3d25.0549462!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x43f7405f5c2b6594!2zMjXCsDAzJzE3LjgiTiAxMjHCsDI3JzI1LjkiRQ!5e0!3m2!1szh-TW!2stw!4v1637550814856!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/THRXYmqYuF4Ava7a6',
   phone: '02-2276-7272',
   fbLink: 'https://www.facebook.com/105760468592626',
   fbMessage: 'https://m.me/105760468592626',
