a9ccdedd209085f7fc2ab36d2ddc93787cb754ff
xcatx <xcatx796@gmail.com>
2021-12-13 10:26:44 +0800

    zdjs

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,10 @@
 </template>
 
 <style lang="scss">
+/* 
 @import url('https://fonts.googleapis.com/css?family=Noto+Serif+TC:200,300,400,500,600,700,900&subset=chinese-traditional');
 @import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC:100,300,400,500,700,900&subset=chinese-traditional');
+*/
 </style>
 
 <script>
