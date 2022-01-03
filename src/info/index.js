13820f651eacfa0712559e8cd151e74892e69ea6
xcatx <xcatx796@gmail.com>
2022-01-03 16:17:05 +0800

    TY

--- src/info/index.js
+++ src/info/index.js
@@ -1,12 +1,12 @@
 // import { isMobile } from '../utils/index'
 export default {
-  address: 'XXXXXX',
+  address: '接待中心:新莊區榮華路二段175號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3611.8256910510745!2d121.7047557150085!3d25.141583283922103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2ce6432f630b323d!2zMjXCsDA4JzI5LjciTiAxMjHCsDQyJzI1LjAiRQ!5e0!3m2!1szh-TW!2stw!4v1637818700801!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/djYoE4CqZaJ8sBy66',
-  phone: 'XXXXXX',
-  fbLink: 'https://www.facebook.com/',
-  fbMessage: 'https://m.me/',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14457.466231717304!2d121.43638426764075!3d25.05556228396224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a958b6d759af%3A0x972cbbc6606ba05f!2z5qqA5oKm!5e0!3m2!1szh-TW!2stw!4v1641195912221!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/cjNftSytEmZnUbEW7',
+  phone: '02-8502-1688',
+  fbLink: 'https://www.facebook.com/zhongyuetanyue',
+  fbMessage: 'https://m.me/zhongyuetanyue',
   caseName: '檀悅',
   indigatorLength: 10,
   caseId: 'tye',
@@ -14,8 +14,8 @@ export default {
   houseInfos: [
     ['投資興建', 'XXX'],
     ['建築設計', 'XXX'],
-    ['公設設計', 'XXX'],
     ['接待中心', ' XXX'],
+    ['企劃銷售', ' 樸新廣告'],
   ],
   gtmCode: ['52SFGQ9'], // 可放置多個
   recaptcha_site_key_v2: '6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC',
