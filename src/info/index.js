076b5a513976a0853aaf03cac1457fb7e201ef43
xcatx <xcatx796@gmail.com>
2022-01-19 09:48:19 +0800

    MAP

--- src/info/index.js
+++ src/info/index.js
@@ -2,8 +2,8 @@
 export default {
   address: '接待會館：新北市新莊區新泰路100號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.936395347399!2d121.44612586303471!3d25.036232503040683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a875b4d5a8f1%3A0x760877076d78d21a!2zMjQy5paw5YyX5biC5paw6I6K5Y2A5paw5rOw6LevMTAwLTHomZ8!5e0!3m2!1szh-TW!2stw!4v1642064274092!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/QMrnEQJa9uSvo3Vh7',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d903.7348827437671!2d121.44794152919255!3d25.036126098998228!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a8744b4f1ef3%3A0xf2c9e36d47ce1071!2zMjQy5paw5YyX5biC5paw6I6K5Y2A5paw5rOw6LevMTAw6Jmf!5e0!3m2!1szh-TW!2stw!4v1642556856577!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/UCQWDTXeLWi2pkM8A',
   phone: '02-2991-2999',
   fbLink: 'https://www.facebook.com/tycoon29912999/',
   fbMessage: 'https://m.me/tycoon29912999',
