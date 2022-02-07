2971778a393d720d083ca7e274fcd5e472acdeca
xcatx <xcatx796@gmail.com>
2022-02-07 09:07:47 +0800

    gtm

--- src/info/index.js
+++ src/info/index.js
@@ -19,7 +19,7 @@ export default {
     ['基地位置', '雲林縣林內鄉九芎村民權路',],
   ],
 
-  gtmCode: ['TQPPZSD'], // 可放置多個
+  gtmCode: ['MW7GBQQ'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
