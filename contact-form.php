c51a5d2fe9aab69070938d19224d4ff4ab80d7fa
xcatx <xcatx796@gmail.com>
2022-03-16 15:53:43 +0800

    xcat

--- contact-form.php
+++ contact-form.php
@@ -1,12 +1,8 @@
 <?php
     $src =$_SERVER['SERVER_NAME'];
-    if (isset($_POST['type']) == 1) {
-    $case_name = "得邦廣告-合作提案";
-        $case_code = 'db-cooperation';
-    } else {
-    $case_name = "得邦廣告-賞屋諮詢";
-        $case_code = "debang";
-    }
+    $case_name= isset($_POST['type']) ? '得邦廣告-合作提案' : '得邦廣告-賞屋諮詢';
+    $case_code = isset($_POST['type']) ? 'db-cooperation' : 'debang';
+
 
 
     $name         = isset($_POST['name']) ? $_POST['name'] : '';
@@ -250,7 +246,7 @@
     $mail->FromName = $case_name." - 官網網站"; //設定寄件者姓名
 
     $mail->Subject = $case_name." - 官網網站"; //設定郵件標題
-    $mail->Body = "網站：https://".$src."/<BR>姓名：".$name."<BR>電話：".$phone."<BR>信箱：".$user_email."<BR>想了解的建案：".$house."<BR>城市：".$city.$area."<BR>留言：".$msg."<BR>可聯絡時間：".$contact_time."<BR><BR>填表日期：".$datetime."<BR>廣告來源：".$utm_source."<BR>廣告媒介：".$utm_medium."<BR>廣告名稱：".$utm_campaign."<BR>廣告內容：".$utm_content; //設定郵件內容
+    $mail->Body = $case_code.$case_name."網站：https://".$src."/<BR>姓名：".$name."<BR>電話：".$phone."<BR>信箱：".$user_email."<BR>想了解的建案：".$house."<BR>城市：".$city.$area."<BR>留言：".$msg."<BR>可聯絡時間：".$contact_time."<BR><BR>填表日期：".$datetime."<BR>廣告來源：".$utm_source."<BR>廣告媒介：".$utm_medium."<BR>廣告名稱：".$utm_campaign."<BR>廣告內容：".$utm_content; //設定郵件內容
     $mail->IsHTML(true); //設定郵件內容為HTML
 
     $tomail_arr = explode(",",$tomail);
