d648fa1fc4fd28d77c7adf07d71aaf8c9ba01089
xcatx <xcatx796@gmail.com>
2022-03-24 14:31:49 +0800

    換價錢

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Noto+Serif+TC:500,900&subset=chinese-traditional');
 @import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC:400,700&subset=chinese-traditional');
+@import url('https://fonts.googleapis.com/css2?family=Biryani:wght@900&family=Lato:wght@900&display=swap');
 </style>
 
 <script>
