aacf240290a4954a85e330359806a38d56729593
xcatx <xcatx796@gmail.com>
2022-07-28 15:13:28 +0800

    11

--- src/info/index.js
+++ src/info/index.js
@@ -1,21 +1,25 @@
 import { isMobile } from '../utils/index'
 export default {
-  address: '新北市淡水區中山北路三段90號',
+  address: '接待會館：新竹縣竹東鎮北興路二段220號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14441.41951953333!2d121.43573446824783!3d25.191252038122155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442baa83ff102a3%3A0x24daeba041b51f27!2zMjUx5paw5YyX5biC5reh5rC05Y2A5Lit5bGx5YyX6Lev5LiJ5q61OTDomZ8!5e0!3m2!1szh-TW!2stw!4v1649669513959!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/Ex9gYvWbgBuNqBZK8',
-  phone: '02-2626-8800',
-  fbLink: 'https://www.facebook.com/105125832094346/',
-  fbMessage: 'https://m.me/105125832094346',
-  caseName: '文學苑',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3623.5640792278596!2d121.0905563!3d24.741841700000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346847c0ff9d9d9f%3A0xfde0c21acf6074e1!2z6Iez5ZaE6auY56ysLeaOpeW-heacg-mkqO-8iOiIiOWQiOWKm-W7uuevieWcmOmaii3llJDoqqDlu7roqK3vvIk!5e0!3m2!1szh-TW!2stw!4v1658989610500!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/wg3ETypf1BikQV16A',
+  phone: '03-595-7999',
+  fbLink: 'https://www.facebook.com/109284968192482/',
+  fbMessage: 'https://m.me/109284968192482',
+  caseName: '至善高第',
   indigatorLength: 10,
 
   houseInfos: [
-    ['投資興建', '鼎翊建設',],
-    ['建築規劃', '劉文岱建築師',],
-    ['建照號碼', '104淡建字第00616號',],
-    ['行銷企劃', '澄果廣告',],
-    ['經紀人', '胡光志（99）嘉市字第00074號',],
+    ['投資興建', '興合力建築團隊-唐誠建設股份有限公司',],
+    ['建築設計', '徐丹和建築師事務所',],
+    ['外觀設計', '原典設計股份有限公司',],
+    ['公設園藝', '和境設計工程有限公司',],
+    ['建照號碼', '(111)府建字第00146號',],
+    ['樓層規劃', '地上14層/地下3層',],
+    ['戶數規劃', '124戶/127車',],
+    ['廣告代銷', '新理想行銷團隊-新理想廣告股份有限公司',],
+    ['建案位置', '新竹縣竹東鎮至善段40~43等4筆地號',],
   ],
 
   gtmCode: ['NBPQ652'], // 可放置多個
