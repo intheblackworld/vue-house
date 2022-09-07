169f6bf40147011c5607bc5cd4a35deca9e945e0
molly <molly8358158@gmail.com>
2022-09-07 14:58:48 +0800

    修改資訊

--- src/info/index.js
+++ src/info/index.js
@@ -2,12 +2,12 @@ import { isMobile } from '../utils/index'
 export default {
   address: '桃園市桃園區民生路107號',
   googleSrc:
-    'https://www.google.com/maps/',
-  googleLink: 'https://goo.gl/maps/',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3616.222840181513!2d121.31118071561177!3d24.992542846104893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34681fab750441c3%3A0xcd1bb3babe8a7fe0!2z55ub6Zuy5bq35a-TIFgg6bS75oWn6ZaL55m8!5e0!3m2!1szh-TW!2stw!4v1662533203629!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/E4BkDfLVnaSBmc91A',
   phone: '03-325-9925',
-  fbLink: 'https://www.facebook.com/',
-  fbMessage: 'https://m.me/',
-  caseName: '自遊城',
+  fbLink: 'https://www.facebook.com/102447382596524/',
+  fbMessage: 'https://m.me/102447382596524/',
+  caseName: '盛雲康寓',
   indigatorLength: 10,
 
   houseInfos: [
@@ -18,7 +18,7 @@ export default {
     ['服務據點', '桃園市桃園區中正路1048號5樓',],
   ],
 
-  gtmCode: ['TJ344HX'], // 可放置多個
+  gtmCode: ['5P6KGCX'], // 可放置多個
   recaptcha_site_key_v2: '6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
