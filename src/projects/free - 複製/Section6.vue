d32ecd6657c5fa880a7ba08395b4648726ffff3e
xcatx <xcatx796@gmail.com>
2022-09-22 11:46:17 +0800

    x

---
+++ src/projects/free - 複製/Section6.vue
@@ -0,0 +1,423 @@
+<template>
+  <div>
+    <div class="section6">
+      <div class="swipe" data-aos="fade" data-aos-delay="200" @mouseenter.stop="toggleTimer = false" @mouseleave.stop="toggleTimer = true">
+        <img src="./s5/light.png" class="light" />
+        <div class="swipe-wrap relative" v-touch:swipe.left="decIndex" v-touch:swipe.right="addIndex">
+          <transition-group name="swipe-fade" mode="out-in">
+            <div v-for="(slide, i) in slideList" v-show="slideIndex === i" :key="slide.img" :class="`swipe-item absolute`">
+              <img :src="slide.img" alt="">
+              <div class="slide-name absolute" v-html="slide.name"></div>
+            </div>
+          </transition-group>
+     <!--  -->    <div class="swipe-btns absolute flex-ac flex-jb" v-if="isMobile">
+            <div class="prev-btn" @click="decIndex">
+            <img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 35 60'%3E%3Cpolyline fill='none' stroke='%23FFF' stroke-width='6' points='31.5,57 4.5,30 31.5,3 '/%3E%3C/svg%3E" alt="_prev">
+            </div>
+            <div class="next-btn" @click="addIndex">
+            <img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 35 60'%3E%3Cpolyline fill='none' stroke='%23FFF' stroke-width='6' points='3.5,3 30.5,30 3.5,57 '/%3E%3C/svg%3E" alt="_next">
+            </div>
+          </div>
+        </div>
+     <!--  --><div class="pagination absolute flex-ac" data-aos="fade-up" data-aos-delay="500" v-if="isPC">
+        <div :class="`pagination-dot`" v-for="(slide, index) in slideList" :key="slide.img + '-dot'" @click="goTo(index)"><span :class="`${slideIndex === index ? 'active' : ''}`"></span></div>
+      </div> 
+      </div>
+      <div class="txt">
+    <div class="cloud">
+    <cloud class="cloud1" />
+    <cloud class="cloud2" />
+    </div>
+      <div class="title" data-aos="fade-up" data-aos-delay="200">便利交通網  行動不受限</div>
+      <div class="desc" data-aos="fade-up" data-aos-delay="300">輕軌新市一路站4分鐘，捷運淡水站6分鐘，公車站牌1分鐘，16條以上路線去哪都方便。 
+</div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import '@/assets/style/function.scss';
+
+.section6 {
+  width:100%;
+  height:auto;
+  position: relative;
+  margin:0 auto 0;
+  padding: 5vw size(198) 4vw;
+&::after{content: "";clear: both;display: block;}
+}
+.cloud{
+  position: absolute;
+  top:4em;right: 0;
+  width: 94%;
+  font-size: size(15);
+  .cloud1{
+  position: absolute;
+  fill: #FFE8BC;
+    width: 5em;
+    top:0em;
+    left: 0em;
+      transform:translateX(110%);
+        animation: cloud 5s linear alternate infinite;
+  }
+  .cloud2{
+  position: absolute;
+  fill: #FFE8BC;
+    width: 2.8em;
+    top: 2.9em;
+    left: 6.8em;
+      transform:translateX(100%);
+        animation: cloud 5s linear alternate infinite;
+  }
+}
+  @keyframes cloud{
+    to{
+      transform:translateX(0);
+    }
+}
+
+.txt{
+float: left;
+  position: relative;
+  width: size(635);
+  height: size(708);
+  padding: 0 size(40);
+  font-stretch: normal;
+  font-style: normal;
+  text-align: justify;
+  font-size: size(19);
+  font-weight: 400;
+  line-height: 1.4;
+  z-index: 1;
+  margin-right: 0;
+  background: #FFF url("./s6/bg.png") no-repeat 0% 100%;
+  border-radius: size(20);
+  display: flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+  }
+
+.title{
+  font-size:2.2em;
+  margin:0 auto 0;
+  color: #444;
+  font-weight: 600;
+  width: 100%;
+  &::after{display: block;
+    height: 2px;content: "";background: #CF5700;margin: 0 auto;width: 100%;
+  }
+}
+  .subtitle{font-size: 1.25em;
+    padding:.75em 0 0 0;
+  }
+.desc {
+ // margin:1em auto;
+  padding:1em 0 0 0;
+  line-height: 1.6;
+  list-style: circle;
+  overflow: hidden;
+  li{list-style:inherit;float: left;margin: 0.3em 0 0.3em 1.4em;width:calc(100% - 1.4em);
+  }
+}
+.light{position: absolute;
+  width: size(100);
+z-index: 3;
+top: size(-35);right: size(-20);
+      animation: light3 2s linear infinite;
+    }
+@keyframes light3{
+    0%{
+      transform:rotate(-90deg) scale(0);opacity: 1;
+    }
+    50%{
+      transform:rotate(0deg)  scale(.8,1.5);opacity: 1;
+    }
+    100%{
+      transform:rotate(90deg) scale(1.5,.8);opacity: 0;
+    }
+}
+
+/* Swipe */
+
+.swipe {position: relative;
+float: right;
+ // position: absolute;
+  width: size(840);
+  height: size(668);
+// top: size(185);
+ // left: size(210);
+  margin-left: 0;
+  z-index: 2;
+}
+.slide-name {
+    right:1.5em;
+    bottom:0;
+    color: #fff;
+    font-size: size(15);
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1;
+    letter-spacing: 0.89px;
+    text-align: left;
+    color: #ffffff;
+   text-shadow:0 0.1em 0.3em #000;
+   z-index: 3;
+}
+
+.swipe-wrap {
+  width: 100%;
+  height: 100%;
+}
+.swipe-item {
+  width: 100%;
+  height: 100%;
+  z-index: 0; 
+  &::before{
+    content: "";
+    position: absolute;
+    top: size(0);right:size(-20);
+    width: 100%;
+    height: calc(100% + #{size(40)});
+    background: #fff5;
+    z-index: 1;
+  border-radius: size(40);
+  }
+  &::after{
+    content: "";
+    position: absolute;
+    top: size(13);right:size(-7);
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(-135deg, rgba(255,255,255,1) 0%,rgba(255,255,255,0) 20%);
+    z-index: 1;
+  border-radius: size(27);
+  }
+  img {position: relative;
+    z-index: 3;top: size(20);
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  border-radius: size(20);
+  }
+}
+
+// 過場動畫
+// begin 
+.swipe-fade-leave-to {
+  opacity: 0;
+  z-index: 0;
+}
+// end
+.swipe-fade-enter {
+  opacity: 0;
+  z-index: 1;
+}
+
+.swipe-fade-enter-active {
+  transition: all 0.5s ease;
+}
+
+.swipe-fade-leave-active {
+  transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+// pagination
+.pagination {
+  width: auto;
+  bottom: size(0);
+  right:calc(100% + 50 * 100vw / 1920);
+  justify-content: center;
+  font-size: size(20);
+
+}
+.pagination-dot {
+  padding: 0.25em;
+  margin: 0 0.2em;
+  cursor: pointer;
+  z-index: 4;
+
+  span {
+    display: block;
+    width:1em;
+    height:1em;
+    border-radius: 50%;
+    border: 0.2em solid  $pagination;
+    position: relative;
+    transition: all 0.5s;
+
+    &::before {
+      content: '';
+      width: 60%;
+      height: 60%;
+      display: block;
+    border-radius: 50%;
+    border:  0.105em solid  $pagination-active;
+      opacity: 1;
+      position: absolute;
+      top: 20%;
+      left: 20%;
+      transition: all 0.3s;
+      transform-origin: center;
+      transform: scale(0);
+    }
+    &.active {
+      box-shadow: none;
+      &::before {
+        width: 100%;
+        height: 100%;
+        top: 0%;
+        left: 0%;
+        transform: scale(1.6);
+      }
+    }
+  }
+}
+.swipe-btns {
+  width: 100%;
+  height: 100%;
+  padding: 0 0.75em;
+  z-index: 3;
+  font-size: size(20);
+
+  .prev-btn,
+  .next-btn {
+    width: 1em;
+    cursor: pointer;
+  }
+}
+
+@media only screen and (max-width: 1440px) {
+}
+@media only screen and (max-width: 1280px) and (min-width: 1025px) {
+  .fullscreen {
+    height: 100vh;
+  }
+}
+
+/* 螢幕尺寸標準 */
+/* 平板尺寸 */
+@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+}
+
+@media screen and (max-width: 767px) {
+  .section6 {
+    width: 100%;
+    height: auto;
+    padding: 0 0 sizem(50) 0;
+  background-size: 250% auto;
+
+  }
+.cloud{
+  font-size: sizem(10);top:1em;
+}
+  .txt{
+    position: relative;
+    width: sizem(315);
+    height: auto;
+    float: none;
+    margin:1.5em auto 0em;
+    padding: 4em 1em 6.8em;
+    font-size: sizem(15);
+    background-position:  100% 100%;
+    border-radius: sizem(10);
+    background-size: sizem(193) auto;
+    .title{
+      font-size:1.37em;
+  &::after{width: 100%;flex: 0 0 100%;
+  display: block;margin: .3em 0 0.3em 0;}
+    }
+    .subtitle{
+      font-size:1.1em;
+    }
+  }
+.light{
+  width: sizem(70);
+top: sizem(-25);left: sizem(-25);
+    }
+  /* Swipe */
+  .swipe {
+    position: relative;
+    width: sizem(315);
+    float: none;
+    height: sizem(250);
+  margin:auto;
+  }
+.swipe-item {
+  &::before{
+    top: 0;left:sizem(-12);
+    width: 100%;
+    height:100%;
+  border-radius: sizem(10);
+  }
+  &::after{
+    top: sizem(7);left:sizem(-5);
+    background: linear-gradient(135deg, rgba(255,255,255,1) 0%,rgba(255,255,255,0) 20%);
+    border-radius: sizem(10);
+  }
+  img {top: sizem(12);
+  border-radius: sizem(10);}
+  .slide-name {
+    font-size: sizem(12);
+  }
+}
+  .swipe-btns {
+  font-size: sizem(15);top: sizem(6);
+  }
+}
+</style>
+<script>
+// @ is an alias to /src
+import info from '@/info'
+import { isPC, isMobile, isTablet } from '@/utils'
+import slider from '@/mixins/slider.js'
+import cloud from './cloud.vue'
+
+export default {
+  name: 'section6',
+  components: {
+    cloud,
+  },
+
+  mixins: [slider],
+  props: ['viewIndex'],
+
+  data() {
+    return {
+      info,
+      isPC,
+      isMobile,
+      isTablet,
+      isDialog: false,
+      slideList: [
+        {
+          img: isMobile?require('./s6/1_m.jpg'):require('./s6/1.jpg'),
+          name: '新市一路站',
+        },
+        {
+          img: isMobile?require('./s6/2_m.jpg'):require('./s6/2.jpg'),
+          name: '捷運淡水站',
+        },
+      ],
+    }
+  },
+
+  methods: {},
+
+  created() {},
+
+  mounted() {},
+
+  computed: {},
+
+  watch: {
+    viewIndex() {
+      if (this.viewIndex === 3) {
+        this.slideIndex = 0
+        console.log(this.slideIndex, 'slideIndex')
+      }
+    },
+  },
+}
+</script>
---
+++ src/projects/free - 複製/Section9.vue
@@ -0,0 +1,326 @@
+<template>
+  <div>
+    <div class="section9">
+      <div class="swipe" data-aos="fade" data-aos-delay="200" @mouseenter.stop="toggleTimer = false" @mouseleave.stop="toggleTimer = true">
+        <div class="swipe-wrap relative" v-touch:swipe.left="decIndex" v-touch:swipe.right="addIndex">
+          <transition-group name="swipe-fade" mode="out-in">
+            <div v-for="(slide, i) in slideList" v-show="slideIndex === i" :key="slide.img" :class="`swipe-item absolute`">
+              <img :src="slide.img" alt="">
+              <div class="slide-name absolute" v-html="slide.name"></div>
+            </div>
+          </transition-group>
+      <!--   -->   <div class="pagination absolute flex-ac" data-aos="fade-up" data-aos-delay="200" v-if="isPC">
+            <div :class="`pagination-dot`" v-for="(slide, index) in slideList" :key="slide.img + '-dot'" @click="goTo(index)"><span :class="`${slideIndex === index ? 'active' : ''}`"></span></div>
+          </div>
+          <div class="swipe-btns absolute flex-ac flex-jb" v-if="isMobile" >
+            <img src="./all/prev-btn.png" alt="" class="prev-btn" @click="decIndex">
+            <img src="./all/next-btn.png" alt="" class="next-btn" @click="addIndex">
+          </div>
+        </div>
+      </div>
+      <div class="txt">
+      <div class="title" data-aos="fade-up" data-aos-delay="200" >從心出發  打造理想住宅</div>
+      <div class="desc" data-aos="fade-up" data-aos-delay="300">近1450坪的大基地，以最佳戶數規劃總戶數171戶社區，輔以豐富公設、綠化造景、親子遊戲空間，在市中心打造最宜居的生活環境，就算不出門，在社區中也能悠遊自在。</div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import '@/assets/style/function.scss';
+
+.section9 {  
+  width:100%;
+  height:auto;
+  position: relative;
+  padding: 4vw 0 4vw;
+  background: url("./s4/city.png") no-repeat 0 80%;
+&::after{content: "";clear: both;display: block;}
+}
+
+.img{width: size(800);position: absolute;left: 0;bottom: size(-130);
+z-index: 2;
+img{width: 100%;
+// transform: skewY(5deg) ;
+// transform-origin: 0 50%;
+  //    animation: an 3s linear alternate infinite;
+      }
+}
+
+@keyframes an{
+    to{
+      transform: translateX(0);
+    }
+}
+.txt{
+  position: relative;
+  width: size(1500);
+  margin:-2em auto 0em auto;
+  padding: 2.8em 3em;
+  font-stretch: normal;
+  font-style: normal;
+  text-align: justify;
+  font-size: size(18);
+  font-weight: 400;
+  line-height: 1.4;
+  z-index: 2;
+  background: #FFF;
+  border-radius: size(20);
+  
+ // color: #fff;
+
+.title{
+  font-size:2em;
+  margin:0 auto 0;
+  color: #444;
+  display: flex;
+    align-items:baseline;
+  font-weight: 600;
+  &::after{flex: 1;
+    height: 2px;content: "";background: #CF5700;margin: 0 0 0 .7em;
+  }
+}
+  .subtitle{font-size: 1.25em;
+    padding:.75em 0 0 0;
+  }
+
+.desc {
+  padding:1em 0 0 0;
+  line-height: 1.6;
+  list-style: circle;
+}
+  }
+/* Swipe */
+.swipe {
+  position: relative;
+  margin: auto;
+  width: size(1500);
+  height: size(753);
+  // left: size(210);
+  object-fit: cover;
+  z-index: 3;
+}
+.slide-name {
+    right:1.5em;
+    bottom:1em;
+    color: #fff;
+    font-size: size(15);
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1;
+    letter-spacing: 0.89px;
+    text-align: left;
+    color: #ffffff;
+   text-shadow:0 0.1em 0.3em #000;
+}
+
+.swipe-wrap {
+  width: 100%;
+  height: 100%;
+}
+.swipe-item {
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  border-radius: size(20);
+  }
+}
+
+// 過場動畫
+// begin 
+.swipe-fade-leave-to {
+  opacity: 0;
+  z-index: 0;
+}
+// end
+.swipe-fade-enter {
+  opacity: 0;
+  z-index: 1;
+}
+
+.swipe-fade-enter-active {
+  transition: all 0.5s ease;
+}
+
+.swipe-fade-leave-active {
+  transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+// pagination
+.pagination {
+  width: auto;
+  bottom: -2.2em;
+  right: 2.5em;
+  justify-content: center;
+  font-size: size(20);
+
+}
+.pagination-dot {
+  padding: 0.25em;
+  margin: 0 0.2em;
+  cursor: pointer;
+  z-index: 4;
+
+  span {
+    display: block;
+    width:1em;
+    height:1em;
+    border-radius: 50%;
+    border: 0.2em solid  $pagination;
+    position: relative;
+    transition: all 0.5s;
+
+    &::before {
+      content: '';
+      width: 60%;
+      height: 60%;
+      display: block;
+    border-radius: 50%;
+    border:  0.105em solid $pagination-active;
+      opacity: 1;
+      position: absolute;
+      top: 20%;
+      left: 20%;
+      transition: all 0.3s;
+      transform-origin: center;
+      transform: scale(0);
+    }
+    &.active {
+      box-shadow: none;
+      &::before {
+        width: 100%;
+        height: 100%;
+        top: 0%;
+        left: 0%;
+        transform: scale(1.6);
+      }
+    }
+  }
+}
+.swipe-btns {
+  width: 100%;
+  height: 100%;
+  padding: 0 0.75em;
+  z-index: 3;
+  font-size: size(20);
+
+  .prev-btn,
+  .next-btn {
+    width: 1em;
+    cursor: pointer;
+  }
+}
+
+@media only screen and (max-width: 1440px) {
+}
+@media only screen and (max-width: 1280px) and (min-width: 1025px) {
+  .fullscreen {
+    height: 100vh;
+  }
+}
+
+/* 螢幕尺寸標準 */
+/* 平板尺寸 */
+@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+}
+
+@media screen and (max-width: 767px) {
+  .section9 {
+  padding: 11vw 0 1vw;
+  background:none;
+  }
+  .img{width: sizem(300);left: sizem(45);bottom: sizem(-50);}
+  .txt{
+    position: relative;
+    width: sizem(345);
+    margin:-2em auto 4em;
+    padding: 3em 1em 1.3em;
+    left:0;
+    top:0;
+    font-size: sizem(15);
+    border-radius: sizem(10);
+    .title{
+      font-size:1.37em;
+      flex-wrap: wrap;
+  &::after{width: 100%;flex: 0 0 100%;
+  display: block;margin: .3em 0 0.3em 0;}
+    }
+    .subtitle{
+      font-size:1.1em;
+    }
+  }
+  /* Swipe */
+  .swipe {
+    position: relative;
+    width: sizem(345);
+    height: sizem(200);
+    top:0;
+    left:0;
+    background: #FFF;
+    border: sizem(15) solid #FFF;
+    border-radius: sizem(10);
+  }
+.swipe-item {
+  img {
+  border-radius: sizem(10);}
+  .slide-name {
+    font-size: sizem(12);
+  }
+}
+  .swipe-btns {
+  font-size: sizem(15);
+  display: none;
+  }
+}
+
+</style>
+<script>
+// @ is an alias to /src
+import info from '@/info'
+import { isPC, isMobile, isTablet } from '@/utils'
+import slider from '@/mixins/slider.js'
+
+export default {
+  name: 'section9',
+
+  mixins: [slider],
+  props: ['viewIndex'],
+
+  data() {
+    return {
+      info,
+      isPC,
+      isMobile,
+      isTablet,
+      isDialog: false,
+      slideList: [
+        {
+          img: isMobile?require('./s9/1_m.jpg'):require('./s9/1.jpg'),
+          name: '3D模擬圖',
+        },
+      ],
+    }
+  },
+
+  methods: {},
+
+  created() {},
+
+  mounted() {},
+
+  computed: {},
+
+  watch: {
+    viewIndex() {
+      if (this.viewIndex === 7) {
+        this.slideIndex = 0
+        console.log(this.slideIndex, 'slideIndex')
+      }
+    },
+  },
+}
+</script>
