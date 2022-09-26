c8b39b3a8269dae9a7444d86bfedb3b63e53c047
molly <molly8358158@gmail.com>
2022-09-26 13:59:08 +0800

    修改資訊

--- src/info/index.js
+++ src/info/index.js
@@ -2,20 +2,20 @@ import { isMobile } from '../utils/index'
 export default {
    address: '接待中心：台南市北區北安路一段314號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7233.216230269212!2d121.44955870556367!3d24.979444400000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xebe5d55c176b7a21!2zMjTCsDU4JzQ2LjAiTiAxMjHCsDI3JzEyLjIiRQ!5e0!3m2!1szh-TW!2stw!4v1653555645240!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/BLMcLg1QMsJrBg1b9',
-  phone: '02-2264-6789',
-  fbLink: 'https://www.facebook.com/hbl.haiwo',
-  fbMessage: 'https://m.me/hbl.haiwo',
-  caseName: '春福安安',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.907524556433!2d120.17508249999999!3d22.990427799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e779fe9013f79%3A0xca3d2dcdce33b1d3!2z5pil56aP5b6h6YK4IOaOpeW-heS4reW_gw!5e0!3m2!1szh-TW!2stw!4v1664171655616!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/3pwsRRVVsKP9vqJG7',
+  phone: '06-293-0333',
+  fbLink: 'https://www.facebook.com/100071163257991/',
+  fbMessage: 'https://m.me/100071163257991/',
+  caseName: '春福御邸',
   indigatorLength: 10,
-  caseId: 'anan',
+  caseId: 'cfyd',
 
   houseInfos: [
     ['投資興建', '新春建設股份有限公司'],
-    ['規劃坪數', '24坪、28坪、36坪'],
-    ['基地面績', '約637.56坪'],
-    ['房型規劃', '2-3房、店面'],
+    ['規劃坪數', '41-52坪'],
+    ['基地面績', '約561坪'],
+    ['房型規劃', '三房、四房'],
   ],
   gtmCode: ['W2BV8MK'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
