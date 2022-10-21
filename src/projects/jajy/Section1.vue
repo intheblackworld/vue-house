5b0d55509e00cb0ef086f7dcf33dc84a0eb7cb8d
molly <molly8358158@gmail.com>
2022-10-21 11:32:24 +0800

    修改

--- src/projects/jajy/Section1.vue
+++ src/projects/jajy/Section1.vue
@@ -1,81 +1,69 @@
 <template>
-  <div>
-    <div class="section1">
-    <wimg class="w1 absolute" v-if="!isMobile" loading="lazy" />
-
-
-      <div class="swipe" data-aos="fade" data-aos-delay="200" @mouseenter.stop="toggleTimer = false" @mouseleave.stop="toggleTimer = true">
-        <div class="swipe-wrap relative" v-touch:swipe.left="decIndex" v-touch:swipe.right="addIndex">
-          <transition-group name="swipe-fade" mode="out-in">
-            <div v-for="(slide, i) in slideList" v-show="slideIndex === i" :key="slide.img" :class="`swipe-item absolute`">
-              <img :src="slide.img" alt="" loading="lazy">
-              <div class="slide-name absolute" v-html="slide.name"></div>
-            </div>
-          </transition-group>
-      <!--   -->   <div class="pagination absolute flex-ac" data-aos="fade-up" data-aos-delay="200" v-if="isPC">
-            <div :class="`pagination-dot`" v-for="(slide, index) in slideList" :key="slide.img + '-dot'" @click="goTo(index)"><span :class="`${slideIndex === index ? 'active' : ''}`"></span></div>
+  <div class="section1">
+    <div class="content">
+      <div class="title" v-html="slideList[slideIndex].title">
+      </div>
+      <div class="subtitle" v-html="slideList[slideIndex].subtitle">
+      </div>
+      <div class="desc" v-html="slideList[slideIndex].desc"></div>
+    </div>
+    <div class="swipe" data-aos="fade" data-aos-delay="200" @mouseenter.stop="toggleTimer = false" @mouseleave.stop="toggleTimer = true">
+      <div class="swipe-wrap relative" v-touch:swipe.left="decIndex" v-touch:swipe.right="addIndex">
+        <transition-group name="swipe-fade" mode="out-in">
+          <div v-for="(slide, i) in slideList" v-show="slideIndex === i" :key="slide.img" :class="`swipe-item absolute`">
+            <img :src="slide.img" alt>
+            <div class="slide-name absolute" v-html="slide.name"></div>
           </div>
-          <div class="swipe-btns absolute flex-ac flex-jb" v-if="isMobile" >
-            <img src="./all/prev-btn.png" alt="" class="prev-btn" @click="decIndex" loading="lazy">
-            <img src="./all/next-btn.png" alt="" class="next-btn" @click="addIndex" loading="lazy">
+        </transition-group>
+        <div v-if="isPC" class="pagination absolute flex-ac" data-aos="fade" data-aos-delay="200">
+          <div :class="`pagination-dot`" v-for="(slide, index) in slideList" :key="slide.img + '-dot'" @click="goTo(index)">
+            <span :class="`${slideIndex === index ? 'active' : ''}`"></span>
           </div>
+        </div>
+        <div class="swipe-btns absolute flex-ac flex-jb">
+          <div class="prev-btn" @click="decIndex"></div>
+          <div class="next-btn" @click="addIndex"></div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
-@import '@/assets/style/function.scss';
+@import "@/assets/style/function.scss";
 
-.section1 {  
-  width:100%;
-  height:auto;
+.section1 {
+  width: 100%;
+  height: 100vh;
   position: relative;
-  padding: 0 0 1.7vw 0;
+ // background: url("./s2/pc.png");
 }
+
+.title {
+  font-weight: bold;
+  color: #E2C46C;
+  font-size:2em;
+}
+
+.subtitle {
+  font-size:1.5em;
+  font-weight: bold;
+  margin: 0 auto .3em;
+}
+
+.desc {
+  font-family:  'Noto Sans TC', sans-serif;
+
+}
+
+
 /* Swipe */
 .swipe {
-  position: relative;
-  margin: auto;
-  width: size(1920);
+  width: 100%;
   height: 100vh;
-  // left: size(210);
-  object-fit: cover;
-  z-index: 3;
-  box-shadow: .3vw .3vw .7vw #0006;
-}
-.slide-name {
-    right:1.5em;
-    bottom:1em;
-    color: #fff;
-    font-size: size(15);
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1;
-    letter-spacing: 0.89px;
-    text-align: left;
-    color: #ffffff;
-   text-shadow:0 0.1em 0.3em #000;
+  z-index: 1;
 }
 
-.swipe-wrap {
-  width: 100%;
-  height: 100%;
-}
-.swipe-item {
-  width: 100%;
-  height: 100%;
-  z-index: 0;
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-}
-
-// 過場動畫
-// begin 
+// begin
 .swipe-fade-leave-to {
   opacity: 0;
   z-index: 0;
@@ -94,68 +82,139 @@
   transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
 }
 
-// pagination
+.swipe-wrap {
+  width: 100%;
+  height: 100%;
+}
+
+.swipe-item {
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+
+  img {
+    width: 100%;
+  }
+
+  .slide-name {
+    font-family: "Noto Sans TC";
+    left: 0;
+    width: 100%;
+    bottom: 1em;
+    color: #fff;
+    font-size: size(18);
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.6;
+    letter-spacing: 0.03em;
+    text-align: center;
+    text-shadow: 0 0.3em 1em #0003 ,0 0 .2em #000;
+  }
+}
+
 .pagination {
   width: auto;
-  bottom: size(20);
-  right: size(960);
+  bottom:size(15);
+  right: 0;
+  left: 0;
+  margin: 0 auto;
   justify-content: center;
   font-size: size(15);
 }
+
 .pagination-dot {
-  padding: 0.25em;
-  margin: 0 0.2em;
+  padding:0.4em;
+  margin: 0 0.4em;
   cursor: pointer;
   z-index: 4;
 
   span {
     display: block;
-    width:1em;
-    height:1em;
-    border-radius: 50%;
-    border:  2px solid  $pagination;
+    width: 0.8em;
+    height:0.8em;
+    border-radius: 0.8em;
     position: relative;
+    background-color: #fff9;
     transition: all 0.5s;
 
     &::before {
-      content: '';
-      width: 60%;
-      height: 60%;
+      content: "";
+      width: 100%;
+      height: 100%;
       display: block;
-    border-radius: 50%;
-   // border:  2px solid  $pagination-active;
-   background: #fff;
+      background: #fff;
+      border-radius: 50%;
       opacity: 1;
       position: absolute;
-      top: 20%;
-      left: 20%;
+      top: 0;
+      // transform: translateY(-50%);
+      left: 0;
       transition: all 0.3s;
-      transform-origin: center;
+      transform-origin: 50% 50%;
       transform: scale(0);
     }
     &.active {
-      box-shadow: none;
       &::before {
-        width: 100%;
-        height: 100%;
-        top: 0%;
-        left: 0%;
-        transform: scale(1.3);
+        transform: scale(1);
       }
     }
   }
 }
+
 .swipe-btns {
   width: 100%;
   height: 100%;
-  padding: 0 0.75em 0 0.75em;
+  padding: 0 0;
   z-index: 3;
-  font-size: size(20);
-
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
   .prev-btn,
   .next-btn {
-    width: 1em;
+    position: relative;
+    height: 100%;
+    width: 2em;
+    font-size: size(20);
     cursor: pointer;
+    &::before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      transform: translateX(100%);
+      background-color: #0004;
+      transition: all 0.3s;
+    }
+    &::after {
+      content: "";
+      width: 1em;
+      height: 1em;
+      position: absolute;
+      top: calc(50% - 0.5em);
+      left: calc(50% - 0.75em);
+      border: solid #fff;
+      border-width: 0.1em 0.1em 0 0;
+      transform: rotate(45deg) translate(-10%, 10%);
+    }
+    &:hover:before {
+      transform: translateX(0%);
+    }
+    &:hover:after {
+      animation: btn 0.5s ease-in-out infinite alternate;
+    }
+  }
+  .prev-btn {
+    transform: scaleX(-1);
+  }
+}
+
+@keyframes btn {
+  to {
+    transform: rotate(45deg) translate(10%, -10%);
   }
 }
 
@@ -173,45 +232,62 @@
 }
 
 @media screen and (max-width: 767px) {
-  .section5 {
+  .section1 {
     width: 100%;
     height: auto;
-    min-height: auto;
-    max-height: initial;
+    min-height: sizem(667);
+    max-height: sizem(667);
   }
+
+
+  .mouse {
+    display: none;
+  }
+
   /* Swipe */
   .swipe {
     position: relative;
     width:sizem(375);
-    height: 100vh;
+    height: sizem(667);
+    margin: 0 auto 0;
     box-shadow: 0.5vw 0.5vw 1.5vw #0006;
   }
-.swipe-item {
-  .slide-name {
-    font-size: sizem(12);
-  }
-}
-  .swipe-btns {
-  font-size: sizem(15);
-  }
-}
+  
 
+
+  .swipe-item {
+    .slide-name {
+      font-size: sizem(12);
+    }
+  }
+
+
+  .swipe-btns {
+    .prev-btn,
+    .next-btn {
+      font-size: sizem(15);
+      background-color: rgba(0, 0, 0, 0);
+      &::before {display: none;
+      }
+      &::after {
+        border-color: #fff;
+        border-width: 0.15em 0.15em 0 0;
+        animation: btn 0.5s ease-in-out infinite alternate;
+      }
+    }
+  }
+}
 </style>
 <script>
 // @ is an alias to /src
-import info from '@/info'
-import { isPC, isMobile, isTablet } from '@/utils'
-import slider from '@/mixins/slider.js'
-import wimg from './w.vue'
+import { isPC, isMobile, isTablet } from "@/utils";
+import slider from "@/mixins/slider.js";
+import info from "@/info";
 
 export default {
-  name: 'section5',
+  name: "section1",
 
   mixins: [slider],
-  props: ['viewIndex'],
-  components: {
-    wimg,
-  },
 
   data() {
     return {
@@ -220,20 +296,34 @@ export default {
       isMobile,
       isTablet,
       isDialog: false,
+      dialogImg: null,
+      showMask: false,
       slideList: [
         {
           img: isMobile?require("./s1/slider/slider_1_m.jpg"):require("./s1/slider/slider_1.jpg"),
-          name: '',
+          name: "",
+          area: "",
         },
         {
           img: isMobile?require("./s1/slider/slider_2_m.jpg"):require("./s1/slider/slider_2.jpg"),
-          name: '',
-        },
-      ],
-    }
+          name: "",
+          area: "",
+        }
+      ]
+    };
   },
 
-  methods: {},
+  methods: {
+    showDialog() {
+      this.isDialog = true;
+      this.showMask = true;
+      this.dialogImg = this.slideList[this.slideIndex].full;
+    },
+
+    handleScroll() {
+      this.showMask = false;
+    }
+  },
 
   created() {},
 
@@ -241,13 +331,6 @@ export default {
 
   computed: {},
 
-  watch: {
-    viewIndex() {
-      if (this.viewIndex === 7) {
-        this.slideIndex = 0
-        console.log(this.slideIndex, 'slideIndex')
-      }
-    },
-  },
-}
+  watch: {}
+};
 </script>
