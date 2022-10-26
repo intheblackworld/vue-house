ab590c47f37d86d8f32557a8ccf1486066b0cc95
xcatx <xcatx796@gmail.com>
2022-10-26 10:27:06 +0800

    great

--- src/info/index.js
+++ src/info/index.js
@@ -4,7 +4,7 @@ export default {
   googleSrc:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1819.0780511478913!2d120.554830658219!3d24.236318441888507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x899de0a84eac7bcd!2zMjTCsDE0JzEwLjciTiAxMjDCsDMzJzIxLjMiRQ!5e0!3m2!1szh-TW!2stw!4v1659516134992!5m2!1szh-TW!2stw',
   googleLink: 'https://goo.gl/maps/dp6LhKL8rcT5EWP59',
-   phone: '04-26621777',
+   phone: '04-2663-1999',
   fbLink: 'https://www.facebook.com/103362852445712',
   fbMessage: 'https://m.me/103362852445712',
   caseName: '',
