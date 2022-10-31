7bd2d295833091a3ed8ecde010de5383314a712c
molly <molly8358158@gmail.com>
2022-10-31 08:22:05 +0800

    增加GTM

--- src/info/index.js
+++ src/info/index.js
@@ -18,7 +18,7 @@ export default {
     ['行銷企劃', '海沃創意行銷',],
   ],
 
-  gtmCode: [''], // 可放置多個
+  gtmCode: ['N4QBMF9'], // 可放置多個
   recaptcha_site_key_v2: '6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
