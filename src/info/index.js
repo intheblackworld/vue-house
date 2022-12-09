84045fe80a0666bdb1a8d551d4c1ab0b0d0faf14
molly <molly8358158@gmail.com>
2022-12-09 13:48:22 +0800

    修改

--- src/info/index.js
+++ src/info/index.js
@@ -4,7 +4,7 @@ export default {
   googleSrc:
     'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3614.3847262875265!2d121.45499929999998!3d25.054946200000003!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a9b321c197c5%3A0x2bb68e2704c516c1!2z5Ymv6YO95b-D6Z2S6Z2SIOaOpeW-heS4reW_gy3mlrDljJcv5paw6I6KL-W7uuahiC_mlrDlu7rmoYgv6aCQ5ZSu5bGLL-aWsOaIkOWxiw!5e0!3m2!1szh-TW!2stw!4v1670319353712!5m2!1szh-TW!2stw',
   googleLink: 'https://goo.gl/maps/y7M89BffH2o9Uq46A',
-  phone: '02-8521-1313',
+  phone: '02-2279-1188',
   fbLink: 'https://www.facebook.com/gtcip.debang',
   fbMessage: 'https://m.me/gtcip.debang',
   caseName: '雙橡園',
