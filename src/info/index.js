afb967c2db4ba3f05da230f11c020410101c2d9f
molly <molly8358158@gmail.com>
2022-12-14 11:16:12 +0800

    增加GTM

--- src/info/index.js
+++ src/info/index.js
@@ -15,7 +15,7 @@ export default {
     ['建築設計', '富翊建築師事務所',],
   ],
 
-  gtmCode: ['W53QTB5'], // 可放置多個
+  gtmCode: ['W53QTB5','PKM73FC'], // 可放置多個
   recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
