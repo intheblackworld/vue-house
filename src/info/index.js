f6021b5a308859e45f8c72adaf17470fab5c09ac
molly <molly8358158@gmail.com>
2023-01-09 09:43:35 +0800

    修改

--- src/info/index.js
+++ src/info/index.js
@@ -2,8 +2,8 @@ import { isMobile } from '../utils/index'
 export default {
   address: '新北市新莊區中原路101號對面',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3614.3847262875265!2d121.45499929999998!3d25.054946200000003!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a9b321c197c5%3A0x2bb68e2704c516c1!2z5Ymv6YO95b-D6Z2S6Z2SIOaOpeW-heS4reW_gy3mlrDljJcv5paw6I6KL-W7uuahiC_mlrDlu7rmoYgv6aCQ5ZSu5bGLL-aWsOaIkOWxiw!5e0!3m2!1szh-TW!2stw!4v1670319353712!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/y7M89BffH2o9Uq46A',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d903.5998810237637!2d121.45594439999999!3d25.0544444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x78f1864717cc01c8!2zMjXCsDAzJzE2LjAiTiAxMjHCsDI3JzIxLjQiRQ!5e0!3m2!1szh-TW!2stw!4v1673227664716!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/yMczBu3XZbYLn2XC9',
   phone: '02-2279-1188',
   fbLink: 'https://www.facebook.com/gtcip.debang',
   fbMessage: 'https://m.me/gtcip.debang',
