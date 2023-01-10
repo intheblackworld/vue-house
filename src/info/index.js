ba4ccbcca0a028161cbabbd44d16680acd53ea7a
xcatx <xcatx796@gmail.com>
2023-01-10 09:53:05 +0800

    客戶GTM

--- src/info/index.js
+++ src/info/index.js
@@ -17,7 +17,7 @@ export default {
     ['坪數規劃', '82、91坪',],
   ],
 
-  gtmCode: ['WNGS5LD'], // 可放置多個
+  gtmCode: ['WNGS5LD','NB8KMN7'], // 可放置多個
   recaptcha_site_key_v2: '6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
