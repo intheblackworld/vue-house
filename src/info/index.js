1af4649497792aa6646937c18782036cbb88820a
xcatx <xcatx796@gmail.com>
2023-01-10 10:05:21 +0800

    gtm

--- src/info/index.js
+++ src/info/index.js
@@ -17,7 +17,7 @@ export default {
     ['坪數規劃', '45-64坪',],
   ],
 
-  gtmCode: ['TPG82D3'], // 可放置多個
+  gtmCode: ['W72HX9F','TPG82D3'], // 可放置多個
   recaptcha_site_key_v2: '6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
