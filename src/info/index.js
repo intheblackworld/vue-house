649d52ee22a6ee42898e3c8dedc3d0cff487d5fe
xcatx <xcatx796@gmail.com>
2023-01-10 10:02:26 +0800

    客戶GTM

--- src/info/index.js
+++ src/info/index.js
@@ -17,7 +17,7 @@ export default {
     ['坪數規劃', '22-24坪',],
   ],
 
-  gtmCode: ['KSHMLZG'], // 可放置多個
+  gtmCode: ['KSHMLZG','PFWNTHV'], // 可放置多個
   recaptcha_site_key_v2: '6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
