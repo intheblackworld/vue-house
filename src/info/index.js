349f5daefc6c637aebe4659f17d9dbc9120dd230
molly <molly8358158@gmail.com>
2023-03-02 13:40:13 +0800

    修改GTM

--- src/info/index.js
+++ src/info/index.js
@@ -17,7 +17,7 @@ export default {
     ['房型規劃', '兩房、三房',],
   ],
 
-  gtmCode: ['PDH8S8G'], // 可放置多個
+  gtmCode: ['NDGMSK5'], // 可放置多個
   recaptcha_site_key_v2: '6LfGUjEaAAAAANYvhZQx5imGm23pRt0V-rOvkpNC',
   recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
   recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
