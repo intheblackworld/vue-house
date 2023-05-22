565bd9f5b8fe711c2f1110793f76e8021c289203
xcatx <xcatx796@gmail.com>
2023-05-22 09:31:27 +0800

    換地址

--- src/info/index.js
+++ src/info/index.js
@@ -1,11 +1,11 @@
 // import { isMobile } from '@/utils'
 
 export default {
-  address: '新北市五股區新城五路與新城八路交叉',
+  address: '新北市五股區新城六路159號',
   // isMobile ? '接待中心：<br />106台北市大安區基隆路三段20-1號' : '接待中心：106台北市大安區基隆路三段20-1號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.293723920597!2d121.44400571738508!3d25.09191669899662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDA1JzMwLjkiTiAxMjHCsDI2JzQ2LjMiRQ!5e0!3m2!1szh-TW!2stw!4v1590128993509!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/wUFoxM69s4RmGV6a6',
+    'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7226.616800755305!2d121.4409764!3d25.0914197!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a8aa1b8531e3%3A0xa9a4897f5a8af782!2zMjQ45paw5YyX5biC5LqU6IKh5Y2A5paw5Z-O5YWt6LevMTU56Jmf!5e0!3m2!1szh-TW!2stw!4v1684718968669!5m2!1szh-TW!2stw',
+  googleLink: 'https://goo.gl/maps/wvbTBjapoEN21LTJ8',
   phone: '02-82951777',
   fbLink:
     'https://www.facebook.com/神采飛洋-101802774887170',
