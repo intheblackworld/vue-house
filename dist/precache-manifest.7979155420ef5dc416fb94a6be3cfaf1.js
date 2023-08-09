8f127ba9a4cb1677f2962a94b6f7ff3bc7bc61bb
molly <molly8358158@gmail.com>
2023-08-09 16:11:06 +0800

    新增影片

---
+++ dist/precache-manifest.7979155420ef5dc416fb94a6be3cfaf1.js
@@ -0,0 +1,678 @@
+self.__precacheManifest = [
+  {
+    "revision": "6286b4100b4ea842ba30",
+    "url": "/css/about.80c7bb21.css"
+  },
+  {
+    "revision": "6286b4100b4ea842ba30",
+    "url": "/js/about.d57c24b7.js"
+  },
+  {
+    "revision": "6f00c606c4366c72e232",
+    "url": "/css/app.a3ee61ea.css"
+  },
+  {
+    "revision": "6f00c606c4366c72e232",
+    "url": "/js/app.0475b9ba.js"
+  },
+  {
+    "revision": "e6a0abeae4538ee27156",
+    "url": "/css/chunk-4ffcd95e.63e821a6.css"
+  },
+  {
+    "revision": "e6a0abeae4538ee27156",
+    "url": "/js/chunk-4ffcd95e.ab61710c.js"
+  },
+  {
+    "revision": "61b385c960a92df9e95c",
+    "url": "/css/chunk-vendors.4d31623c.css"
+  },
+  {
+    "revision": "61b385c960a92df9e95c",
+    "url": "/js/chunk-vendors.82534b9c.js"
+  },
+  {
+    "revision": "98712d5a998fc693b4db",
+    "url": "/css/contacts.eec854f9.css"
+  },
+  {
+    "revision": "98712d5a998fc693b4db",
+    "url": "/js/contacts.4ae7fad4.js"
+  },
+  {
+    "revision": "890bdc928ce21176be13",
+    "url": "/css/hot.bb099b3d.css"
+  },
+  {
+    "revision": "890bdc928ce21176be13",
+    "url": "/js/hot.5080167b.js"
+  },
+  {
+    "revision": "cd3e0456adfb6f9474c1",
+    "url": "/css/news.c06647ee.css"
+  },
+  {
+    "revision": "cd3e0456adfb6f9474c1",
+    "url": "/js/news.4d5be047.js"
+  },
+  {
+    "revision": "fdc467239310e7573514",
+    "url": "/css/products.e9553cef.css"
+  },
+  {
+    "revision": "fdc467239310e7573514",
+    "url": "/js/products.ca877c5e.js"
+  },
+  {
+    "revision": "6702fd464f6ae7e7f1ea",
+    "url": "/css/thanks.3a464db9.css"
+  },
+  {
+    "revision": "6702fd464f6ae7e7f1ea",
+    "url": "/js/thanks.84360755.js"
+  },
+  {
+    "revision": "5fbd99e5b481f378c7b56bc4d4c5f8dc",
+    "url": "/img/1.5fbd99e5.jpg"
+  },
+  {
+    "revision": "967daec3424956c0c9a19ddd3c4d39b8",
+    "url": "/img/4.967daec3.jpg"
+  },
+  {
+    "revision": "43fc50900ec4b9b8d3ba4d287d7646af",
+    "url": "/img/1.43fc5090.jpg"
+  },
+  {
+    "revision": "6dd587dc7d0c809ff2f47eaa87d67e7e",
+    "url": "/img/title.6dd587dc.png"
+  },
+  {
+    "revision": "948ccb99c7d46e23c330787c673fcb08",
+    "url": "/img/bg.948ccb99.jpg"
+  },
+  {
+    "revision": "4cc856ee8d79882ba4e4a3fb56901cda",
+    "url": "/img/icon.4cc856ee.png"
+  },
+  {
+    "revision": "c5af053acecfa18efae2439d9d32916d",
+    "url": "/img/1.c5af053a.jpg"
+  },
+  {
+    "revision": "0c75aafb6acea7a326e4eb1512ee70cf",
+    "url": "/img/經濟1.0c75aafb.jpg"
+  },
+  {
+    "revision": "756ceb18db0f26f37f8cb6dc75bf616f",
+    "url": "/img/0.756ceb18.jpg"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3f794132d3b27c470814b844b9e5d6eb",
+    "url": "/img/4.3f794132.jpg"
+  },
+  {
+    "revision": "705c1c7dc926d598ff385908c6573294",
+    "url": "/img/4.705c1c7d.jpg"
+  },
+  {
+    "revision": "bd10b486d6998148fc6180308d41a810",
+    "url": "/img/20201103002310.bd10b486.jpg"
+  },
+  {
+    "revision": "1b1285c0b4fca48cf7b8dadbb314b3b3",
+    "url": "/img/經濟2.1b1285c0.jpg"
+  },
+  {
+    "revision": "5ba839a31959e9515a0ead246965d9b5",
+    "url": "/img/3e6e77c8-960a-1bf1-ea24-310cedcf7ef3.5ba839a3.jpg"
+  },
+  {
+    "revision": "c5a7fc59e4267504d7a781f0be607d78",
+    "url": "/img/三立2.c5a7fc59.jpg"
+  },
+  {
+    "revision": "bb6bd25023515dde5abb6c23674cce6a",
+    "url": "/img/contact-logo.bb6bd250.jpg"
+  },
+  {
+    "revision": "a99d19fcc197413fa1317f144d53712e",
+    "url": "/img/5.a99d19fc.jpg"
+  },
+  {
+    "revision": "88470ac7992e0300d7f9ed093de8e85d",
+    "url": "/img/i_m.88470ac7.jpg"
+  },
+  {
+    "revision": "89b3a41533ca6454665a08eb3a0b3f29",
+    "url": "/img/logo.89b3a415.png"
+  },
+  {
+    "revision": "d2e0f4f43d34a863ec70f08f39561893",
+    "url": "/img/中時3.d2e0f4f4.jpg"
+  },
+  {
+    "revision": "71c9d41692be4d08ce9b76179cf9f909",
+    "url": "/img/三立5.71c9d416.jpg"
+  },
+  {
+    "revision": "e63ae84bb2fe4fbe11d35d00f585cbff",
+    "url": "/img/中時2.e63ae84b.jpg"
+  },
+  {
+    "revision": "30015c9adb407e2fb55a1caa9657135d",
+    "url": "/img/0.30015c9a.jpg"
+  },
+  {
+    "revision": "d3e8227d53e48fcddf69241cf7dd39b6",
+    "url": "/img/3.d3e8227d.jpg"
+  },
+  {
+    "revision": "0e3da75397a096f60accea06951c67cf",
+    "url": "/img/20210330002199.0e3da753.jpg"
+  },
+  {
+    "revision": "6d2f5af8cf880e7119131e0eb3eb5462",
+    "url": "/img/4.6d2f5af8.jpg"
+  },
+  {
+    "revision": "c5a38274c8b5d7ebe8d3c65e3dc7f445",
+    "url": "/img/title.c5a38274.png"
+  },
+  {
+    "revision": "d3005df1f1578df935bb0726efd2636b",
+    "url": "/img/3.d3005df1.jpg"
+  },
+  {
+    "revision": "abc4c4bf0d22c7c05f6b52f4d152ac47",
+    "url": "/img/i.abc4c4bf.jpg"
+  },
+  {
+    "revision": "7032d94c1be8f81f483bed81567266e1",
+    "url": "/img/loading_b.7032d94c.gif"
+  },
+  {
+    "revision": "cfce6abbbff0099b15691345d8b94dcc",
+    "url": "/fonts/gothic.cfce6abb.ttf"
+  },
+  {
+    "revision": "79e91fbd0179e73cb7c91c3a044f44b4",
+    "url": "/img/thank-img-hover.79e91fbd.png"
+  },
+  {
+    "revision": "83d50008f5091fbe9d19c3921afdb73c",
+    "url": "/img/1.83d50008.jpg"
+  },
+  {
+    "revision": "f5571184551dbe4bb1ade030733df863",
+    "url": "/img/5.f5571184.jpg"
+  },
+  {
+    "revision": "b76793e268a2e64ccae12a0652caa31a",
+    "url": "/img/bg.b76793e2.jpg"
+  },
+  {
+    "revision": "687e5f4b332a1f4bd482e310501af5bb",
+    "url": "/img/3.687e5f4b.jpg"
+  },
+  {
+    "revision": "5df8590f0d394495f439a5465bdf8ffd",
+    "url": "/img/中時4.5df8590f.jpg"
+  },
+  {
+    "revision": "96e6d1836b75a0ef51899c1f8f3a6d90",
+    "url": "/img/3.96e6d183.jpg"
+  },
+  {
+    "revision": "54c01cea10de199c32b3505211a1d9e6",
+    "url": "/img/2.54c01cea.jpg"
+  },
+  {
+    "revision": "ac08272eca387504d66873ef4f78dc54",
+    "url": "/img/2.ac08272e.jpg"
+  },
+  {
+    "revision": "2f9f61b42bc057e36b088fd469652339",
+    "url": "/img/thank_bg.2f9f61b4.png"
+  },
+  {
+    "revision": "b04ae92481e519912a31fb20533e69c4",
+    "url": "/img/3.b04ae924.jpg"
+  },
+  {
+    "revision": "03302c8e658a24f33c2e08c22fcd8cbf",
+    "url": "/img/1.03302c8e.jpg"
+  },
+  {
+    "revision": "9fc85c434563eb861b5b6fac93364aec",
+    "url": "/img/1.9fc85c43.jpg"
+  },
+  {
+    "revision": "d324aad553603cfdb2b831bd67790255",
+    "url": "/img/3.d324aad5.jpg"
+  },
+  {
+    "revision": "b8476907179686fdcc31973e4852bf47",
+    "url": "/img/2.b8476907.jpg"
+  },
+  {
+    "revision": "3b9d0b9245bcf7c5be880a42c33c96f7",
+    "url": "/img/2.3b9d0b92.jpg"
+  },
+  {
+    "revision": "ffa4880fca42a07deeda44b26685c3b4",
+    "url": "/img/title.ffa4880f.png"
+  },
+  {
+    "revision": "55bd2a4a59063dca564171517b92eaaa",
+    "url": "/img/title.55bd2a4a.png"
+  },
+  {
+    "revision": "3db3f54c33df0470feafde04a090516e",
+    "url": "/img/20201103002431.3db3f54c.jpg"
+  },
+  {
+    "revision": "4653a0911b01a047d49108f74185336d",
+    "url": "/img/中時4.4653a091.jpg"
+  },
+  {
+    "revision": "dfdc548669d04755fb0dd26096aac840",
+    "url": "/img/2.dfdc5486.jpg"
+  },
+  {
+    "revision": "e087cfae56b584d6b8c701b3e78c8611",
+    "url": "/img/baa57624-44c5-5bbf-f771-cf9a5e62efbb.e087cfae.jpg"
+  },
+  {
+    "revision": "b89936d66fa83f8dcbfc0bebb413800e",
+    "url": "/img/1.b89936d6.jpg"
+  },
+  {
+    "revision": "4bb2cb23c2691f24b3284bb3c8676ddb",
+    "url": "/img/1.4bb2cb23.jpg"
+  },
+  {
+    "revision": "02f8361d9d565e18cc6fb8527fdb8a17",
+    "url": "/img/5.02f8361d.jpg"
+  },
+  {
+    "revision": "550d8ff9dc3a4aff9742646c1b7a8a92",
+    "url": "/img/2.550d8ff9.jpg"
+  },
+  {
+    "revision": "5390b90ecf559f3af1785214ea7691ab",
+    "url": "/img/4.5390b90e.jpg"
+  },
+  {
+    "revision": "1556953b463651f023b16aa0602de63f",
+    "url": "/img/6f3caf5e598be1d1546667293708423d.1556953b.jpg"
+  },
+  {
+    "revision": "fcd912b298936763961d4057de019b49",
+    "url": "/img/3.fcd912b2.jpg"
+  },
+  {
+    "revision": "bc5db507455af26d8217a7b66535a0cc",
+    "url": "/img/bg_m.bc5db507.jpg"
+  },
+  {
+    "revision": "e90a810c8c7fd3f3ad73546cc1e6a374",
+    "url": "/img/db_text.e90a810c.png"
+  },
+  {
+    "revision": "cf8f6480fcf3f62cff091cc0661481eb",
+    "url": "/img/5.cf8f6480.jpg"
+  },
+  {
+    "revision": "f43f05fa76897e7c4b51538e3fd1efe5",
+    "url": "/img/4.f43f05fa.jpg"
+  },
+  {
+    "revision": "f980b4f30361a98000d0feac21674a99",
+    "url": "/img/title.f980b4f3.png"
+  },
+  {
+    "revision": "01e689fd6110f23fcced364e15a6e1ac",
+    "url": "/img/20201103002320.01e689fd.jpg"
+  },
+  {
+    "revision": "42eefe264cf496688fbb07f667331bd7",
+    "url": "/img/中時1.42eefe26.jpg"
+  },
+  {
+    "revision": "2b1fdb0de13e003ee2ee7d7c6cf2e6bd",
+    "url": "/img/i1.2b1fdb0d.jpg"
+  },
+  {
+    "revision": "37e6518ee758b2a52f9814a84931f7e1",
+    "url": "/img/bg.37e6518e.jpg"
+  },
+  {
+    "revision": "9b1a371953a41c07fe85d80bc62fe4a0",
+    "url": "/img/1.9b1a3719.jpg"
+  },
+  {
+    "revision": "21272654e0f22513d36cf6d164762210",
+    "url": "/img/footerLogo.21272654.gif"
+  },
+  {
+    "revision": "74dbe936ae5f7608c776756ce1a653be",
+    "url": "/img/3.74dbe936.jpg"
+  },
+  {
+    "revision": "153ab2837b44c11267aca6d605511cdb",
+    "url": "/img/三立3.153ab283.jpg"
+  },
+  {
+    "revision": "a8407af64754b10777c328844973aafa",
+    "url": "/img/中時2.a8407af6.jpg"
+  },
+  {
+    "revision": "0580af941224f153fab6dd80c2c0743f",
+    "url": "/img/7.0580af94.jpg"
+  },
+  {
+    "revision": "3c705fcd79dabb280a2cc8152bc0df18",
+    "url": "/img/v.3c705fcd.jpg"
+  },
+  {
+    "revision": "c1bf5065e2615aa12a31960b04ee9cfc",
+    "url": "/img/7V44J6V6SZFVFE3E5B7TN2BJWA.c1bf5065.jpg"
+  },
+  {
+    "revision": "20303eb1b7e277ba6dda15af03993180",
+    "url": "/img/1.20303eb1.jpg"
+  },
+  {
+    "revision": "dbeb8873ecb4e511752860397c27ece2",
+    "url": "/img/7.dbeb8873.jpg"
+  },
+  {
+    "revision": "69cc9f3dcb2570eec2c1c4fae166c9e7",
+    "url": "/img/6.69cc9f3d.jpg"
+  },
+  {
+    "revision": "e5d50c488a910d3438d052b3b03cf10d",
+    "url": "/img/3.e5d50c48.jpg"
+  },
+  {
+    "revision": "421cda3c640abee10093195425edfbec",
+    "url": "/img/t_m.421cda3c.png"
+  },
+  {
+    "revision": "2fae35ea049ec66f07e918df7db9a3ef",
+    "url": "/img/3.2fae35ea.jpg"
+  },
+  {
+    "revision": "ce976f1335ae52b607a3685b34eb9488",
+    "url": "/img/2.ce976f13.jpg"
+  },
+  {
+    "revision": "20ac42cd7615a3aac66672b5aa68172b",
+    "url": "/img/2.20ac42cd.jpg"
+  },
+  {
+    "revision": "7d4600b1aef8aff90379873d030c3166",
+    "url": "/img/i.7d4600b1.jpg"
+  },
+  {
+    "revision": "302a1aada177cd4c06ee80f9c81da5e9",
+    "url": "/img/0.302a1aad.jpg"
+  },
+  {
+    "revision": "2175f39c38059b3ae7210ce5d7549995",
+    "url": "/img/4.2175f39c.jpg"
+  },
+  {
+    "revision": "1a67064c63ed517c5be77cbab5aa0433",
+    "url": "/img/t.1a67064c.png"
+  },
+  {
+    "revision": "458ed5867ccfd79006089e089aaaf2fc",
+    "url": "/img/bg.458ed586.jpg"
+  },
+  {
+    "revision": "6b0dfcebbd2789b926ee50b5c7b93376",
+    "url": "/img/0.6b0dfceb.jpg"
+  },
+  {
+    "revision": "29a20b1d676693687f963c5ec7c377e8",
+    "url": "/img/2.29a20b1d.jpg"
+  },
+  {
+    "revision": "c6f4c959afe8344134702fb23e5bc4ce",
+    "url": "/img/2.c6f4c959.jpg"
+  },
+  {
+    "revision": "a72458d20139ff9ae38f244cb4ac3cbe",
+    "url": "/img/404-bg.a72458d2.png"
+  },
+  {
+    "revision": "83b895381b36762de13a44776987de43",
+    "url": "/img/404-bg-m.83b89538.png"
+  },
+  {
+    "revision": "8afae083407c0ce1aa15cdff20227523",
+    "url": "/img/1.8afae083.jpg"
+  },
+  {
+    "revision": "9a121385aa1efdc33b95b6faf699dea7",
+    "url": "/img/1.9a121385.jpg"
+  },
+  {
+    "revision": "7439fd099939632f4e936f1f9d244357",
+    "url": "/img/bg.7439fd09.jpg"
+  },
+  {
+    "revision": "c459aecca1655f69a725b57065eed928",
+    "url": "/img/bg.c459aecc.jpg"
+  },
+  {
+    "revision": "14e03f84f664b175b5f7eb3679d90060",
+    "url": "/img/0.14e03f84.jpg"
+  },
+  {
+    "revision": "4cfd516279c26662d4a9eed5ba61a85a",
+    "url": "/img/bg.4cfd5162.jpg"
+  },
+  {
+    "revision": "b1075dff70507927cca4a82fac4ac50b",
+    "url": "/img/1.b1075dff.jpg"
+  },
+  {
+    "revision": "fb3a256a53a5faf7257ef835bf22ad14",
+    "url": "/img/4.fb3a256a.jpg"
+  },
+  {
+    "revision": "a64a350d84dbb21fcf93849cb9f03bf5",
+    "url": "/img/title.a64a350d.png"
+  },
+  {
+    "revision": "4a3e41ce96067eaeffdaed5fed51f11c",
+    "url": "/img/4.4a3e41ce.jpg"
+  },
+  {
+    "revision": "c145e5c7bdf9cc762f0044638f737a9d",
+    "url": "/img/title.c145e5c7.png"
+  },
+  {
+    "revision": "faaada2dd5f908ffb76b134af308b3b9",
+    "url": "/img/title.faaada2d.png"
+  },
+  {
+    "revision": "ba287f36f921cd598cce5449ed05a4c9",
+    "url": "/img/bg.ba287f36.jpg"
+  },
+  {
+    "revision": "60c51c5406955ca82f97e4db4e32e9cb",
+    "url": "/img/20201103002318.60c51c54.jpg"
+  },
+  {
+    "revision": "27e3d6e7888ea7fef0ebf1f24cb1a6e5",
+    "url": "/img/3.27e3d6e7.jpg"
+  },
+  {
+    "revision": "584071af7bcbaa3a43a8ebf365781d48",
+    "url": "/img/1.584071af.jpg"
+  },
+  {
+    "revision": "380948b7072aa1127db3b38e02154ea1",
+    "url": "/img/6.380948b7.jpg"
+  },
+  {
+    "revision": "89a48a6fcf489669a7a6c307eead857c",
+    "url": "/img/bg_m.89a48a6f.jpg"
+  },
+  {
+    "revision": "31f12b3095c0d30bc9d96c9a2b8232f5",
+    "url": "/img/0.31f12b30.jpg"
+  },
+  {
+    "revision": "3d61f8be2dd4a89c5756455a2c6f2afb",
+    "url": "/img/20210330002197.3d61f8be.jpg"
+  },
+  {
+    "revision": "ad6df4f15ecf59bef61cf1c21f6616e9",
+    "url": "/img/thank-img.ad6df4f1.png"
+  },
+  {
+    "revision": "227860bae958e972fb364ba860c054a4",
+    "url": "/img/2.227860ba.jpg"
+  },
+  {
+    "revision": "7977818c288d5dd7a58753f6068f8d1c",
+    "url": "/img/i_m.7977818c.jpg"
+  },
+  {
+    "revision": "042a159f4784bf6339c97b2eeb589ecb",
+    "url": "/img/3.042a159f.jpg"
+  },
+  {
+    "revision": "85f942f79d765b8f87626365a43c09a8",
+    "url": "/img/2.85f942f7.jpg"
+  },
+  {
+    "revision": "f33bec1c86d0e89042619fa706e666a4",
+    "url": "/img/三立7.f33bec1c.jpg"
+  },
+  {
+    "revision": "6cf0f3cd550dd1904ecf87e2367a6209",
+    "url": "/img/bg.6cf0f3cd.jpg"
+  },
+  {
+    "revision": "1581edbc9d8abecc9a92f7be9968e077",
+    "url": "/img/三立4.1581edbc.jpg"
+  },
+  {
+    "revision": "a06f1ab767de5ab7986fd7ec3f39e022",
+    "url": "/img/0.a06f1ab7.jpg"
+  },
+  {
+    "revision": "7afe44baf16df30c143433d683282359",
+    "url": "/img/4.7afe44ba.jpg"
+  },
+  {
+    "revision": "a6c3542e963cf59781d78c3b648d33ad",
+    "url": "/img/title.a6c3542e.png"
+  },
+  {
+    "revision": "8c1396b92eeef537a174b54853af0abd",
+    "url": "/img/6ec896a7b0061345193adef53d31dd25.8c1396b9.jpg"
+  },
+  {
+    "revision": "d6efb6242b93cd9e230fa4a3f8025e54",
+    "url": "/img/3.d6efb624.jpg"
+  },
+  {
+    "revision": "866606bb5e93c41825a28cd1eebb073b",
+    "url": "/img/title.866606bb.png"
+  },
+  {
+    "revision": "6951a2db45119de9916b5d53657ebdb7",
+    "url": "/img/中時1.6951a2db.jpg"
+  },
+  {
+    "revision": "df5259a4871cbf0f618d56ab01791ebf",
+    "url": "/img/3c47049f966d810b27760fa1a50b93fc.df5259a4.jpg"
+  },
+  {
+    "revision": "3206e5a796c339189c93d127f31cf877",
+    "url": "/img/中時3.3206e5a7.jpg"
+  },
+  {
+    "revision": "01789dc6a72f0f37440ff96aa145410f",
+    "url": "/img/1.01789dc6.jpg"
+  },
+  {
+    "revision": "c0885bbd9864d3c070a41f5a1e639535",
+    "url": "/img/2.c0885bbd.jpg"
+  },
+  {
+    "revision": "42b49f9f417fba8a1427aeafa0f5216a",
+    "url": "/img/title_m.42b49f9f.png"
+  },
+  {
+    "revision": "93374ad62a10f7c42ceeca1c2c9a71f4",
+    "url": "/img/title.93374ad6.png"
+  },
+  {
+    "revision": "0effa7319740b617370a31b8f1894069",
+    "url": "/img/東森.0effa731.jpg"
+  },
+  {
+    "revision": "d4260299774b193f2c7c2c0939630f34",
+    "url": "/img/bg.d4260299.jpg"
+  },
+  {
+    "revision": "efc717f6f6717a59c64c882a47dc8332",
+    "url": "/img/6.efc717f6.jpg"
+  },
+  {
+    "revision": "8e52c489ac173e514eda2694afd0d1be",
+    "url": "/img/5.8e52c489.jpg"
+  },
+  {
+    "revision": "f96d9371d2a8d92ef6d3bb46d5dd8a0e",
+    "url": "/img/1.f96d9371.jpg"
+  },
+  {
+    "revision": "c35a398ef97a270a3f2caa629f44e2e4",
+    "url": "/img/3.c35a398e.jpg"
+  },
+  {
+    "revision": "21e5fd684de13e93f83d88076c196bfd",
+    "url": "/img/三立1.21e5fd68.jpg"
+  },
+  {
+    "revision": "154a04c8d862b2aeb1ff631f4b56d740",
+    "url": "/img/2.154a04c8.jpg"
+  },
+  {
+    "revision": "cdd102a8aa1752962e58242f542c7168",
+    "url": "/img/1.cdd102a8.jpg"
+  },
+  {
+    "revision": "63492c0dc2884a6a1b0fa667c26fb48b",
+    "url": "/img/4.63492c0d.jpg"
+  },
+  {
+    "revision": "fac0f0bbd6376b7c2f11759275acc2cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "014d300b529e9f43199f8f49f31c2518",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
--- src/info/shin-lan/media.js
+++ src/info/shin-lan/media.js
@@ -2,14 +2,22 @@
 export const medias = [
   {
     index: 0,
+    title: '「欣聯和心」地段為王 銷售僅半年完銷',
+    desc: '位在中和區的「欣聯和心」，日前舉行開工動土。',
+    img: require('@/projects/shin-lan/shin-lan/video/欣聯建設.jpg'),
+    video: '',
+    id: 'kmqpyA95ypk', // youtube video  link id
+  },
+  {
+    index: 1,
     title: '欣聯信義麗心 劉伊心開箱影片',
-    desc: '信義區最奢華開箱∣市府站300米【信義麗心】大道璀璨地標14‧17‧29坪飯店設計寓所',
+    desc: '信義區最奢華開箱∣市府站300米【信義麗心】大道璀璨地標14‧17‧29坪飯店設計寓所。',
     img: require('@/projects/shin-lan/shin-lan/video/信義麗心2.jpg'),
     video: '',
     id: 'YWnZjO4Nhp8', // youtube video  link id
   },
   {
-    index: 1,
+    index: 2,
     title: '欣聯信義麗心',
     desc: '市府站300米 飯店設計精品寓所，信義CBD輕奢入境14 17 29坪。',
     img: require('@/projects/shin-lan/shin-lan/video/信義麗心.jpg'),
@@ -17,7 +25,7 @@ export const medias = [
     id: 'ohr8B82O_QY', // youtube video  link id
   },
   {
-    index: 2,
+    index: 3,
     title: '欣聯心',
     desc: '位於新店區的欣聯心，鄰近國道3號及雙捷運七張站、大坪林站的絕佳位置，家樂福與裕隆城在旁，生活機能成熟。',
     img: require('@/projects/shin-lan/shin-lan/video/欣聯心.jpg'),
@@ -25,7 +33,7 @@ export const medias = [
     id: '1nIA_oBOMgU', // youtube video  link id
   },
   {
-    index: 3,
+    index: 4,
     title: '欣聯鉅星',
     desc: '「欣聯鉅星」座落於新店區正25米寶橋路上。<br />旗艦級大器規劃、寬敞舒適，80~500坪多元空間，宜廠宜辦。',
     img: require('@/projects/shin-lan/shin-lan/video/欣聯鉅星.jpg'),
@@ -33,14 +41,14 @@ export const medias = [
     id: 'rH_2P40hfvA', // youtube video  link id
   },
   {
-    index: 4,
+    index: 5,
     title: '東園綻',
     desc: '民國107年由欣聯建設開發投資興建，近華江高中及西園國小，生活機能方便。',
     img: require('@/projects/shin-lan/shin-lan/video/東園綻.jpg'),
     id: 'WvQcxArp6J4', // youtube video  link id
   },
   {
-    index: 5,
+    index: 6,
     title: '幸福好境',
     desc: '50米微距離<br />萬坪公園深呼吸',
     img: require('@/projects/shin-lan/shin-lan/video/幸福好境.jpg'),
