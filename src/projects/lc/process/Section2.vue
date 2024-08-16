3caa16128a9e9524be4f343b937328e830479491
xcatx <xcatx796@gmail.com>
2024-08-16 09:57:28 +0800

    工程進度

--- src/projects/lc/process/Section2.vue
+++ src/projects/lc/process/Section2.vue
@@ -956,6 +956,17 @@ export default {
           {
             o_title: '2023 琢渼',
             o_subtitle: '熱銷中 ，預計2027年初完工',
+            img: require('../process/list/31/1.jpg'),
+            title: '2024/07',
+            subtitle: ' 構台完成',
+            isFinished: true,
+            imgs: [
+            require('../process/list/31/1.jpg'),
+            require('../process/list/31/2.jpg'),
+            require('../process/list/31/3.jpg'),
+            ]
+          },
+          {
             img: require('../process/list/28/1.jpg'),
             title: '2024/04',
             subtitle: ' 擋土工程進場',
@@ -969,6 +980,17 @@ export default {
           {
             o_title: '2023 琢壹',
             o_subtitle: '已完售，預計2026年初完工',
+            img: require('../process/list/30/1.jpg'),
+            title: '2024/08',
+            subtitle: '二樓底版灌漿',
+            isFinished: true,
+            imgs: [
+              require('../process/list/30/1.jpg'),
+              require('../process/list/30/2.jpg'),
+              require('../process/list/30/3.jpg'),
+            ]
+          },
+          {
             img: require('../process/list/29/1.jpg'),
             title: '2024/05',
             subtitle: '一樓底版灌漿',
