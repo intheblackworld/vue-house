36e50da01797e9c164760bfeb0f51bc96b9576bd
xcatx <xcatx796@gmail.com>
2024-09-25 14:33:53 +0800

    工程更新

--- src/projects/lc/process/Section2.vue
+++ src/projects/lc/process/Section2.vue
@@ -981,7 +981,7 @@ export default {
             o_title: '2023 琢壹',
             o_subtitle: '已完售，預計2026年初完工',
             img: require('../process/list/32/1.jpg'),
-            title: '2024/08',
+            title: '2024/09',
             subtitle: '三樓底版灌漿',
             isFinished: true,
             imgs: [
