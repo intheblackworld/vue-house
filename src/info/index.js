10a4424bb54b6f3efda20ce78e204cc138d69520
xcatx <xcatx796@gmail.com>
2024-10-22 14:19:38 +0800

    地址

--- src/info/index.js
+++ src/info/index.js
@@ -1,9 +1,9 @@
 import { isMobile } from '../utils/index'
 export default {
-  address: '新北市泰山區明志路二段278號',
+  address: '新北市泰山區明志路二段242號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1807.3103280291143!2d121.42606182936099!3d25.046944600000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a7c638dfad8f%3A0x84e60da1b2cb298b!2zMjQz5paw5YyX5biC5rOw5bGx5Y2A5piO5b-X6Lev5LqM5q61Mjc46Jmf!5e0!3m2!1szh-TW!2stw!4v1660267112435!5m2!1szh-TW!2stw',
-  googleLink: 'https://goo.gl/maps/zQXELpaYR5y5Ej2d8',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.590227102805!2d121.42436197607884!3d25.047976737727325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a7c62072606d%3A0xddffbb36181214c6!2zMjQz5paw5YyX5biC5rOw5bGx5Y2A5piO5b-X6Lev5LqM5q61MjQy6Jmf!5e0!3m2!1szh-TW!2stw!4v1729577871584!5m2!1szh-TW!2stw',
+  googleLink: 'https://maps.app.goo.gl/eb1mcuvjU3HDBxD18',
   phone: '02-2904-6888',
   fbLink: 'https://www.facebook.com/103007875810421',
   fbMessage: 'https://m.me/103007875810421',
