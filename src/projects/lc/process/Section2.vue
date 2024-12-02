3e8f8a9556431e2c48c9be5ef284d7c4f60cf074
xcatx <xcatx796@gmail.com>
2024-12-02 13:55:49 +0800

    更新工程

--- src/projects/lc/process/Section2.vue
+++ src/projects/lc/process/Section2.vue
@@ -983,6 +983,17 @@ export default {
           {
             o_title: '2023 琢壹',
             o_subtitle: '已完售，預計2026年初完工',
+            img: require('../process/list/35/1.jpg'),
+            title: '2024/11',
+            subtitle: '六樓底版灌漿',
+            isFinished: true,
+            imgs: [
+              require('../process/list/35/1.jpg'),
+              require('../process/list/35/2.jpg'),
+              require('../process/list/35/3.jpg'),
+            ]
+          },
+          {
             img: require('../process/list/34/1.jpg'),
             title: '2024/11',
             subtitle: '五樓底版灌漿',
