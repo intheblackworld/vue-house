d82d8397cdb47ec73fbe2dc5197a3117f480ec19
xcatx <xcatx796@gmail.com>
2025-09-19 17:42:26 +0800

    更新工程

--- src/projects/lc/process/Section2.vue
+++ src/projects/lc/process/Section2.vue
@@ -959,6 +959,17 @@ export default {
           {
             o_title: '2023 琢渼',
             o_subtitle: '熱銷中 ，預計2027年初完工',
+            img: require('../process/list/49/1.jpg'),
+            title: '2025/9',
+            subtitle: '三樓底版完成',
+            isFinished: true,
+            imgs: [
+            require('../process/list/49/1.jpg'),
+            require('../process/list/49/2.jpg'),
+            require('../process/list/49/3.jpg'),
+            ]
+          },
+          {
             img: require('../process/list/48/1.jpg'),
             title: '2025/8',
             subtitle: '二樓底版完成',
